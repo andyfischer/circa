172e9dd3ebd26ea4a5dee497229bc8897d6512ac
Paul Hodge <paul.hodge.email@gmail.com>
2007-11-05 05:34:13 -0800

    .

--- src/Term.py
+++ src/Term.py
@@ -5,12 +5,38 @@ class Term(object):
 
     m.function = None
     m.inputs = []
+    m.users = {}
     m.training_info = None
 
     m.setInputs(initial_inputs)
 
   def setInputs(m, list):
-    old_inputs = 
+    old_inputs = m.inputs
+
+    m.inputs = list[:]
+
+    # add to user lists
+    for input in m.inputs:
+      input.users.add(m)
+
+    # prune old list
+    for input in old_inputs:
+      input.pruneUsers()
+
+  def inputsContain(m, term):
+    for input in m.inputs:
+      if input == term: return True
+    return false
+
+  # pruneUsers: Removes any terms from our user list that are not really using us
+  def pruneUsers(m):
+    for user in m.users:
+      if not user.inputsContain(m):
+        m.users.remove(user)
+
+
+
+
 
 
 
