18f0fafcef8b277d304cb9353ebe7d4d588a246a
Andy Fischer <andy.fischer@gmail.com>
2007-11-04 19:57:33 -0800

    initial commit

---
+++ src/Function.py
@@ -0,0 +1,15 @@
+
+
+class Function(object):
+  def __init__(m):
+    
+    # default options
+    m.pureFunction = False
+    m.numBranches = 0
+    m.numTermPointers = 0
+
+    m.setInputs(initial_inputs)
+
+  def evaluate(m, term):
+    pass
+
