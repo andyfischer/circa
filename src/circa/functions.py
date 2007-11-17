16d1d7f3e1935813a91a83b6a2ddaa95f95d9154
Andy Fischer <andy.fischer@gmail.com>
2007-11-17 15:56:33 -0800

    .

--- src/circa/functions.py
+++ src/circa/functions.py
@@ -1,4 +1,4 @@
-import types
+import data_types
 
 
 class BaseFunction(object):
@@ -8,8 +8,8 @@ class BaseFunction(object):
     m.pureFunction = False
     m.numBranches = 0
     m.numTermPointers = 0
-    m.inputType = types.NONE
-    m.outputType = types.NONE
+    m.inputType = data_types.NONE
+    m.outputType = data_types.NONE
 
     m.init()
 
@@ -22,21 +22,13 @@ class BaseFunction(object):
   def makeState(m):
     return None
 
-builtin = {}
-
-def registerFuncsInModule(module):
-  for name in dir(module):
-    obj = getattr(module, name)
-    if type(obj) == types.ClassType and issubclass(obj, BaseFunction):
-      print "found func:" + str(obj)
-      func = obj()
-      global builtin
-      builtin[func.name] = func
-
+# built-in functions
 
 import builtin_functions.math
-import subroutine
 
+add = builtin_functions.math.Add()
+sub = builtin_functions.math.Sub()
+mult = builtin_functions.math.Mult()
+div = builtin_functions.math.Div()
 
-registerFuncsInModule(builtin_functions.math)
-registerFuncsInModule(subroutine)
+subroutine = BaseFunction()
