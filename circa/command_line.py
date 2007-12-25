696be6111f5a725b3ca15916e43c5ec2f1eb2bba
Paul Hodge <paul.hodge.email@gmail.com>
2007-12-24 20:28:25 -0500

    added command line

---
+++ circa/command_line.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+
+def print_usage():
+  print "Usage..."
+
+
+def main():
+
+  # parse the command-line arguments
+  args = sys.argv[:]
+
+  command_arg = args.pop(0)
+
+  if not args:
+    print_usage()
+    return
+
+  files = []
+
+  while args:
+
+    arg = args[0]
+
+    if arg[0] == '-':
+      option_str = arg[1:]
+
+      print "Unrecognized option: " + str(option_str)
+      return
+
+    files.append(arg)
+
+    args.pop(0)
+
+  print "Running files: " + str(files)
+
+
+if __name__ == '__main__':
+  main()
