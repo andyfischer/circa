8585eb5faed0f1758d6800a33794ee34d7792bc0
Paul Hodge <paul.hodge.email@gmail.com>
2008-01-14 06:20:23 -0800

    started builtins file

--- circa/test_expression.py
+++ circa/test_expression.py
@@ -16,6 +16,12 @@ class Test(unittest.TestCase):
     node = parse_to_ast("1 + 2")
     self.assertTrue( type(node) == Infix )
     self.assertTrue( node.token.match == PLUS )
+
+    node = parse_to_ast("add(1,2)")
+    self.assertTrue( type(node) == Function )
+    self.assertTrue( type(node.args[0]) == Literal )
+    self.assertTrue( node.args[0].value == 1 )
+    self.assertTrue( node.args[1].value == 2 )
     
 
 if __name__ == '__main__':
