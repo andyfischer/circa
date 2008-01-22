c092dd73cad1d4d2e756d2e3993e2212f74d09b3
Paul Hodge <paul.hodge.email@gmail.com>
2008-01-21 19:38:38 -0800

    Changed type handling

--- circa/test_signature.py
+++ circa/test_signature.py
@@ -1,19 +1,24 @@
 
 import pdb
 
+import ca_types
 import dsl
 import unittest
 import signature
+
+INT = ca_types.INT
+FLOAT = ca_types.FLOAT
+STRING = ca_types.STRING
 
 pretend_term = dsl.placeholder
 
 class Test(unittest.TestCase):
   def testSimple(self):
-    sig = signature.fixed(int)
+    sig = signature.fixed(INT)
     self.assertTrue(sig.satisfiedBy(pretend_term(1)))
 
   def testSpecific(self):
-    sig = signature.fixed(int, float, str)
+    sig = signature.fixed(INT, FLOAT, STRING)
     self.assertTrue(sig.satisfiedBy(pretend_term(1, 3.4, 'blah')))
     self.assertTrue(sig.satisfiedBy(pretend_term(1, 3.4, 'blah')))
     self.assertFalse(sig.satisfiedBy(pretend_term(1.0, 3.4, 'blah')))
@@ -21,7 +26,7 @@ class Test(unittest.TestCase):
     self.assertFalse(sig.satisfiedBy(pretend_term(1, 3.4, 1)))
 
   def testVarargs(self):
-    sig = signature.varargs(int)
+    sig = signature.varargs(INT)
     self.assertTrue(sig)
     self.assertTrue(sig.satisfiedBy(pretend_term(1)))
     self.assertTrue(sig.satisfiedBy(pretend_term(1,1)))
