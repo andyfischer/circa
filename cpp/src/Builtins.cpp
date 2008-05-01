a12fd5b5dbe655e5759c098a10b7c00b64beaa37
Paul Hodge <paul.hodge.email@gmail.com>
2008-04-30 17:58:57 -0700

    Fix stuff that Scons deleted

--- cpp/src/Builtins.cpp
+++ cpp/src/Builtins.cpp
@@ -9,38 +9,44 @@
 namespace builtins {
 
 static codeunit::CodeUnit* KERNEL = NULL;
+codeunit::CodeUnit* BUILTINS = NULL;
 
-void bootstrap()
+Term* CONST_GENERATOR = NULL;
+Term* TYPE_TYPE = NULL;
+Term* INT_TYPE = NULL;
+Term* STRING_TYPE = NULL;
+
+void bootstrap_kernel()
 {
    KERNEL = new codeunit::CodeUnit;
 
    // Create constant-generator function. This function takes a Type as
    // input, and gives a Function as output.
-   Term* constFuncGenerator = codeunit::bootstrap_empty_term(KERNEL);
-   function::initialize_data(constFuncGenerator);
-   function::set_name(constFuncGenerator, "constant-generator");
+   CONST_GENERATOR = codeunit::bootstrap_empty_term(KERNEL);
+   function::initialize_data(CONST_GENERATOR);
+   function::set_name(CONST_GENERATOR, "constant-generator");
    
    // Create constant-Type function. This function outputs a Type.
    Term* constType = codeunit::bootstrap_empty_term(KERNEL);
-   constType->function = constFuncGenerator;
+   constType->function = CONST_GENERATOR;
    function::initialize_data(constType);
    function::set_name(constType, "constant-Type");
 
    // Create Type type
-   Term* typeType = codeunit::bootstrap_empty_term(KERNEL);
-   typeType->function = constType;
-   codeunit::bind_name(KERNEL, typeType, "Type");
-   type::initialize_data_for_types(typeType);
-   type::set_initialize_data_func(typeType, type::initialize_data_for_types);
+   TYPE_TYPE = codeunit::bootstrap_empty_term(KERNEL);
+   TYPE_TYPE->function = constType;
+   codeunit::bind_name(KERNEL, TYPE_TYPE, "Type");
+   type::initialize_data_for_types(TYPE_TYPE);
+   type::set_initialize_data_func(TYPE_TYPE, type::initialize_data_for_types);
 
    // Implant the Type type
-   codeunit::set_input(KERNEL, constType, 0, typeType);
-   function::set_input_type(constFuncGenerator, 0, typeType);
-   function::set_output_type(constType, typeType);
+   codeunit::set_input(KERNEL, constType, 0, TYPE_TYPE);
+   function::set_input_type(CONST_GENERATOR, 0, TYPE_TYPE);
+   function::set_output_type(constType, TYPE_TYPE);
 
    // Create constant-Function function, which outputs Functions.
    Term* constFuncFunc = codeunit::bootstrap_empty_term(KERNEL);
-   constFuncFunc->function = constFuncGenerator;
+   constFuncFunc->function = CONST_GENERATOR;
    function::initialize_data(constFuncFunc);
    function::set_name(constFuncFunc, "constant-Function");
 
@@ -51,12 +57,28 @@ void bootstrap()
    codeunit::bind_name(KERNEL, funcType, "Function");
 
    // Implant constant-Function
-   codeunit::set_input(KERNEL, constFuncGenerator, 0, constFuncFunc);
+   codeunit::set_input(KERNEL, CONST_GENERATOR, 0, constFuncFunc);
    function::set_output_type(constFuncFunc, funcType);
 
    // Implant Function type
    codeunit::set_input(KERNEL, constFuncFunc, 0, funcType);
-   function::set_output_type(constFuncGenerator, funcType);
+   function::set_output_type(CONST_GENERATOR, funcType);
+}
+
+void bootstrap_builtins()
+{
+    // Create primitives
+
+    // Create int type
+    INT_TYPE = codeunit::create_term(BUILTINS, CONST_GENERATOR, TermList(TYPE_TYPE));
+    type::set_initialize_data_func(INT_TYPE, int_type::initialize_data);
+    type::set_to_string_func(INT_TYPE, int_type::to_string);
+
+    // Create string type
+    STRING_TYPE = codeunit::create_term(BUILTINS, CONST_GENERATOR, TermList(TYPE_TYPE));
+    type::set_initialize_data_func(STRING_TYPE, string_type::initialize_data);
+    type::set_to_string_func(STRINGINT_TYPE, string_type::to_string);
+
 }
 
 }
