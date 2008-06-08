95d4a84b9a94dfbcdedc949add930d96ec49827a
Paul Hodge <paul.hodge.email@gmail.com>
2008-06-08 16:26:12 -0700

    More sophisticated error handling

--- Circa/debug/__init__.py
+++ Circa/debug/__init__.py
@@ -1,6 +1,8 @@
 
 import pdb
 
+from Circa.common import errors
+
 def _assert(condition):
    if not condition:
       pdb.set_trace() # An assertion has failed
@@ -9,3 +11,6 @@ def fail(message):
     print 'Debug.fail:', message
     pdb.set_trace()
 
+def assertType(object, expectedType, variableName):
+    if not isinstance(object, expectedType):
+        raise errors.TypeCheckFail(variableName, expectedType)
