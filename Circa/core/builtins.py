c4f7802969da2bc6fe20a1c41238a1d6d23cfe30
Paul Hodge <paul.hodge.email@gmail.com>
2008-06-11 18:09:55 -0700

    Added 'getGlobal' function

--- Circa/core/builtins.py
+++ Circa/core/builtins.py
@@ -2,3 +2,16 @@
 
 KERNEL_LOADED = False
 
+KERNEL = None
+
+LOADED_MODULES = {}
+
+def getGlobal(name):
+    if KERNEL.definesName(name):
+        return KERNEL.getNamed(name)
+
+    for module in LOADED_MODULES.values():
+        if module.definesName(name):
+            return module.getNamed(name)
+
+    return None
