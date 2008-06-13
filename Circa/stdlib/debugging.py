f98b9cfe133686fc84884525f65028dbbac5acd1
Andy Fischer <andy.fischer@gmail.com>
2008-06-13 10:29:27 -0700

    Added Print function

--- Circa/stdlib/debugging.py
+++ Circa/stdlib/debugging.py
@@ -14,5 +14,17 @@ class Assert(function_builder.BaseFunction):
         if not cxt.input(0):
             print "Assert failed on term " + cxt.caller().getIdentifier()
 
+class Print(function_builder.BaseFunction):
+    name = "print"
+    inputTypes = [builtins.STRING_TYPE]
+    outputType = builtins.VOID_TYPE
+    pureFunction = False
+    hasState = False
+
+    @staticmethod
+    def evaluate(cxt):
+        print cxt.input(0)
+
 def createFunctions(codeUnit):
     function_builder.createFunction(codeUnit, Assert)
+    function_builder.createFunction(codeUnit, Print)
