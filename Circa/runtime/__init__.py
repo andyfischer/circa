d99fe13fdf016dcb0cb2435aed50b769b4316fea
Andy Fischer <andy.fischer@gmail.com>
2008-06-18 09:45:47 -0700

    Moved map to 'containers', added map test

--- Circa/runtime/__init__.py
+++ Circa/runtime/__init__.py
@@ -5,14 +5,14 @@
 #   Basic math functions
 #   Debugging functions
 
-import boolean, comparison, debugging, feedback, python, simple_math, map, meta, variables
+import boolean, comparison, containers, debugging, feedback, python, simple_math, meta, variables
 
 def createFunctions(codeUnit):
     boolean.createFunctions(codeUnit)
     comparison.createFunctions(codeUnit)
+    containers.createFunctions(codeUnit)
     debugging.createFunctions(codeUnit)
     feedback.createFunctions(codeUnit)
-    map.createFunctions(codeUnit)
     meta.createFunctions(codeUnit)
     python.createFunctions(codeUnit)
     simple_math.createFunctions(codeUnit)
