007f14a9dff2a4aa840ec1d47dfd40d191b8fbf8
Andy Fischer <andy.fischer@gmail.com>
2008-06-19 09:21:40 -0700

    Code cleanup in 'term'

--- Circa/core/term.py
+++ Circa/core/term.py
@@ -14,24 +14,39 @@ nextGlobalID = 1
 
 class Term(object):
     def __init__(self):
-        "Use 'createTerm' instead of calling this constructor"
+        "Don't use this constructor directly, use CodeUnit.createTerm instead"
    
+        # Inputs, a list of terms
         self.inputs = []
+
+        # Function
         self.functionTerm = None
+
+        # A set of terms that are using us. This includes terms that
+        # have us as an input, and terms that have us as the function.
         self.users = set()
 
-        # The output value of this term
+        # The output value of this term.
         self.cachedValue = None
 
         # True if 'cachedValue' is out of date
         self.needsUpdate = True
 
+        # Persistent state.
         self.state = None
-        self.codeUnit = None
+
+        # If we have an internal branch, this is a list of terms.
+        # Considered to be part of our state.
         self.branch = None
+
+        # The CodeUnit object that owns us
+        self.codeUnit = None
+        
+        # The node in an AST used to create this term
+        self.ast = None
+
         self.givenName = None
         self.debugName = None
-        self.ast = None
 
         self.executionContext = TermExecutionContext(self)
    
@@ -51,8 +66,10 @@ class Term(object):
         return self.codeUnit.getIdentifier(self)
   
     def update(self):
-        # Functions with side effects should not run during an
-        # update() call.
+        """
+        Calls our function and updates this term's value.
+        If this function has side effects, this call does nothing.
+        """
         if not ca_function.pureFunction(self.functionTerm):
             return
 
@@ -60,28 +77,54 @@ class Term(object):
         self.needsUpdate = False
 
     def execute(self):
+        "Calls our function and updates our output value"
         ca_function.evaluateFunc(self.functionTerm)(self.executionContext)
         self.needsUpdate = False
 
     def execute_trace(self):
+        """
+        Calls our function using the Python debugger (pdb). Useful for
+        debugging.
+        """
         pdb.runcall(ca_function.evaluateFunc(self.functionTerm), self.executionContext)
         self.needsUpdate = False
   
     # value accessors
     def __int__(self):
-        try: return int(self.pythonValue)
-        except: return 0
+        """
+        Access our value as an integer. Throws a TypeError if this term does
+        not output an integer. This call does no coersion.
+        """
+        if self.getType() != builtins.INT_TYPE:
+            raise TypeError()
+
+        return int(self.cachedValue)
   
     def __float__(self):
-        try: return float(self.pythonValue)
-        except: return 0.0
+        """
+        Access our value as a float. Throws a TypeError if this term does
+        not output a float. This call does no coersion.
+        """
+        if self.getType() != builtins.FLOAT_TYPE:
+            raise TypeError()
+
+        return float(self.cachedValue)
   
     def __str__(self):
-        return ca_type.toStringFunc(self.getType())(self)
+        """
+        Access our value as a string. Throws a TypeError if this term does
+        not output a string. This call does no coersion.
+        """
+        if self.getType() != builtins.STRING_TYPE:
+            raise TypeError()
+
+        return str(self.cachedValue)
 
 class TermExecutionContext(object):
     """
-    This object is provided to evaluate functions when updating.
+    This object is provided to evaluate functions. The purpose is to be
+    able to track when these calls occur, and hope to provide replacements
+    for them.
     """
     def __init__(self, term):
         self.targetTerm = term
