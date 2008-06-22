eb78346b153f87fec5eca683cb5afe7ddca55142
Andy Fischer <andy.fischer@gmail.com>
2008-06-22 12:14:59 -0700

    Added functions for reading/writing whole text files

--- Circa/runtime/__init__.py
+++ Circa/runtime/__init__.py
@@ -2,7 +2,8 @@
 # Contains standard Circa symbols
 #
 
-import boolean, comparison, containers, debugging, feedback, python, ca_math, meta, variables
+import boolean, comparison, containers, debugging
+import feedback, files, python, ca_math, meta, variables
 
 def createFunctions(codeUnit):
     boolean.createFunctions(codeUnit)
@@ -10,6 +11,7 @@ def createFunctions(codeUnit):
     containers.createFunctions(codeUnit)
     debugging.createFunctions(codeUnit)
     feedback.createFunctions(codeUnit)
+    files.createFunctions(codeUnit)
     meta.createFunctions(codeUnit)
     python.createFunctions(codeUnit)
     ca_math.createFunctions(codeUnit)
