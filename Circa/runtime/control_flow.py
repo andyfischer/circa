99515c6f6b680c8eaebea26e54554b4185f45568
Andy Fischer <andy.fischer@gmail.com>
2008-06-23 18:25:38 -0700

    Added if-expr

---
+++ Circa/runtime/control_flow.py
@@ -0,0 +1,44 @@
+
+# Defines various binary functions
+
+import pdb
+from Circa.common import function_builder
+from Circa.core import builtins
+
+class IfStatement(object):
+    name = 'if-statement'
+    inputs = ['bool']
+    output = 'void'
+    pure = False
+
+    def initialize(self):
+        self.branches = []
+
+    def evaluate(self, condition):
+        try:
+            if condition:
+                for term in self.branches[0]:
+                    term.execute()
+            else:
+                for term in self.branches[1]:
+                    term.execute()
+        except IndexError:
+            pass
+
+class IfExpression(object):
+    name = 'if-expr'
+    inputs = ['bool', 'ref', 'ref']
+    output = 'ref'
+    pure = True
+
+    def evaluate(condition, a, b):
+        if condition:
+            return a
+        else:
+            return b
+
+
+def createFunctions(codeUnit):
+    builtins.IF_STATEMENT = function_builder.importPythonFunction(codeUnit,
+            IfStatement, instanceBased = True)
+    function_builder.importPythonFunction(codeUnit, IfExpression)
