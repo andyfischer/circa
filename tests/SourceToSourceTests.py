ae94fd5899d9b5403846d8de75bf3da834fe6693
Paul Hodge <paul.hodge.email@gmail.com>
2008-07-06 00:01:07 -0700

    SourceToSourceTests is working

--- tests/SourceToSourceTests.py
+++ tests/SourceToSourceTests.py
@@ -1,2 +1,19 @@
 
+import os
+import test_helper
+
+
 def sourceToSourceTest(filename):
+    file = open(filename, 'r')
+    original_file = file.read()
+    file.close()
+
+    actual_output = test_helper.runCommand("circa module-to-source.ca " + filename)
+
+    result = test_helper.testSame(original_file, actual_output)
+
+    if result is not None:
+        print "Source-to-source error in file: " + filename
+        print result
+
+sourceToSourceTest("module-to-source.ca")
