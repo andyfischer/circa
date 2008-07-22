ba4109faa0c2d6c6a9beaff9c7d43f5a2bfc200d
Paul Hodge <paul.hodge.email@gmail.com>
2008-07-22 09:59:17 -0700

    execute() is now a non-instance function

--- c_port/src/main.cpp
+++ c_port/src/main.cpp
@@ -12,31 +12,31 @@ void run()
     as_int(five) = 5;
 
     Term* five_as_string = create_term(GetGlobal("to-string"), TermList(five));
-    five_as_string->execute();
+    execute(five_as_string);
     std::cout << "five = " << as_string(five_as_string) << std::endl;
 
     Term* my_struct_def = create_constant(GetGlobal("StructDefinition"));
     my_struct_def = apply_function(GetGlobal("struct-definition-set-name"),
             TermList(my_struct_def, constant_string("my-struct")));
-    my_struct_def->execute();
+    execute(my_struct_def);
 
     my_struct_def = apply_function(GetGlobal("add-field"),
             TermList(my_struct_def, constant_string("myInt"), GetGlobal("int")));
-    my_struct_def->execute();
+    execute(my_struct_def);
     my_struct_def = apply_function(GetGlobal("add-field"),
             TermList(my_struct_def, constant_string("myString"), GetGlobal("string")));
-    my_struct_def->execute();
+    execute(my_struct_def);
 
     Term* my_instance = apply_function(my_struct_def, TermList());
-    my_instance->execute();
+    execute(my_instance);
 
     my_instance = apply_function(GetGlobal("set-field"),
             TermList(my_instance, constant_string("myInt"), constant_int(2)));
-    my_instance->execute();
+    execute(my_instance);
 
     Term* hopefully_two = apply_function(GetGlobal("get-field"),
             TermList(my_instance, constant_string("myInt")));
-    hopefully_two->execute();
+    execute(hopefully_two);
     
     std::cout << "hopefully two = " << hopefully_two->toString() << std::endl;
 }
--- c_port/src/operations.h
+++ c_port/src/operations.h
@@ -12,6 +12,7 @@ void change_type(Term* term, Term* type);
 void specialize_type(Term* term, Term* type);
 void set_input(Term* term, int index, Term* input);
 void set_inputs(Term* term, TermList inputs);
+void execute(Term* term);
 
 // Examine 'function' and 'inputs' and returns a result term. A few things
 // may happen here:
