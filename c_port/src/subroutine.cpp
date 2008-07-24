d08fdbced95d0fdfd00d84de748c2d9ed26cb1df
Andy Fischer <andy.fischer@gmail.com>
2008-07-24 11:47:45 -0700

    Reorg, move subroutine's initializer

--- c_port/src/subroutine.cpp
+++ c_port/src/subroutine.cpp
@@ -1,6 +1,8 @@
 
+#include "branch.h"
 #include "builtins.h"
 #include "errors.h"
+#include "operations.h"
 #include "subroutine.h"
 #include "term.h"
 
@@ -15,4 +17,9 @@ Subroutine* as_subroutine(Term* term)
 void Subroutine_alloc(Term* term)
 {
     term->value = new Subroutine;
+}
+
+void initialize_subroutine(Branch* kernel)
+{
+    BUILTIN_SUBROUTINE_TYPE = quick_create_type(KERNEL, "Subroutine", Subroutine_alloc, NULL);
 }
