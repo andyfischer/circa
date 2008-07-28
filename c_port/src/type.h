8e9d6ed7ed804c850adc27503ce49aee5f9ee305
Andy Fischer <andy.fischer@gmail.com>
2008-07-28 09:10:53 -0700

    Cleanup. Add memberFunctions to Type

--- c_port/src/type.h
+++ c_port/src/type.h
@@ -3,20 +3,31 @@
 
 #include <string>
 
+#include "term_namespace.h"
+
 struct Term;
 
 struct Type
 {
     typedef void (*AllocFunc)(Term* term);
+    typedef void (*DeallocFunc)(Term* term);
     typedef void (*CopyFunc)(Term* src, Term* dest);
 
     std::string name;
 
-    Type();
+    // Code
     AllocFunc alloc;
     CopyFunc copy;
+    DeallocFunc dealloc;
+
+    // memberFunctions is a list of Functions which 'belong' to this type.
+    // They are guaranteed to take a term of our type as their first
+    // argument.
+    TermNamespace memberFunctions;
 
     Term* toString;
+
+    Type();
 };
 
 extern "C" {
