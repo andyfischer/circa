b05e895d7743fee2cf5af6825aa8d935b2455454
Andy Fischer <andy.fischer@gmail.com>
2008-08-17 23:18:23 -0700

    Hunting bug

--- c_port/src/tests/subroutine_test.cpp
+++ c_port/src/tests/subroutine_test.cpp
@@ -28,10 +28,12 @@ void test_using_subroutine_append()
     Branch* branch = new Branch();
     
     quick_exec_function(branch, "test-sub = subroutine-create('test-sub, list(int,int), int)");
+
     quick_exec_function(branch, "test-sub = subroutine-name-inputs(test-sub,list('a,'b))");
     quick_exec_function(branch, "a = subroutine-get-local(test-sub,'a)");
     quick_exec_function(branch, "b = subroutine-get-local(test-sub,'b)");
 
+    quick_exec_function(branch, "print(subroutine-print(test-sub))");
     quick_exec_function(branch, "append-result = subroutine-append(test-sub, mult, list(a,4))");
     quick_exec_function(branch, "test-sub = get-field(append-result, 'sub)");
     quick_exec_function(branch, "a-times-4 = get-field(append-result, 'term)");
@@ -47,6 +49,8 @@ void test_using_subroutine_append()
     quick_exec_function(branch, "test-sub = subroutine-bind(test-sub, sum, 'output)");
 
     quick_exec_function(branch, "write-text-file('sub-append-test, export-graphviz(test-sub))");
+
+    quick_exec_function(branch, "print(subroutine-print(test-sub))");
 
     // Finally, run it
     //std::cout << quick_exec_function(branch, "test-sub(2,3)")->toString();
