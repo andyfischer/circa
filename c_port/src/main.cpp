a2a6e10a1781d2b75b5210af49440111e6b9f041
Paul Hodge <paul.hodge.email@gmail.com>
2008-08-17 18:50:18 -0700

    Added read-text-file

--- c_port/src/main.cpp
+++ c_port/src/main.cpp
@@ -9,7 +9,10 @@ using namespace circa;
 void run()
 {
     Branch* branch = new Branch();
-    quick_exec_function(branch, "write-text-file(\"output.txt\", \"hello\")");
+    quick_exec_function(branch, "s = read-text-file('test)");
+    quick_exec_function(branch, "s2 = concat(s, \" there\")");
+    quick_exec_function(branch, "print(s2)");
+    quick_exec_function(branch, "write-text-file('test, s2)");
 }
 
 int main(int nargs, const char * args[])
