49114250d342f4291c8b3499cfefeea8f50cce25
Andy Fischer <andy.fischer@gmail.com>
2008-08-19 11:14:09 -0700

    Added evaluator.cpp (untested)

---
+++ c_port/src/evaluator.h
@@ -0,0 +1,32 @@
+#ifndef CIRCA__EVALUATOR__INCLUDED
+#define CIRCA__EVALUATOR__INCLUDED
+
+#include "common_headers.h"
+
+namespace circa {
+
+class Evaluator {
+
+    struct Scope {
+        Term* callingTerm;
+        Branch* branch;
+        int next;
+
+        Scope() : callingTerm(NULL), branch(NULL), next(0) {}
+        virtual void onClose() {}
+    };
+
+    struct SubroutineScope : public Scope {
+        virtual void onClose();
+    };
+
+    std::stack<Scope> mStack;
+
+public:
+    void runOneInstruction();
+
+};
+
+}
+
+#endif
