b8ea42ee90b856ec4e1454df5ac73b45a607ae59
Paul Hodge <paul.hodge.email@gmail.com>
2008-08-19 21:28:45 -0400

    Added evaluator test

--- src/tests/subroutine_test.cpp
+++ src/tests/subroutine_test.cpp
@@ -4,6 +4,7 @@
 
 #include "circa.h"
 #include "common.h"
+#include "evaluator.h"
 
 namespace circa {
 namespace subroutine_test {
@@ -40,7 +41,22 @@ void test_using_subroutine_eval()
 
 void test_using_evaluator()
 {
+    Branch* branch = new Branch();
 
+    quick_exec_function(branch, "sub = subroutine-create('test-sub, list(int,int), int)");
+    quick_exec_function(branch, "sub = subroutine-name-inputs(sub, list('a,'b))");
+    quick_exec_function(branch, "sub = subroutine-eval(sub, \"a-times-four = mult(a,4)\")");
+    quick_exec_function(branch, "sub = subroutine-eval(sub, \"b-times-three = mult(b,3)\")");
+    quick_exec_function(branch, "sub = subroutine-eval(sub, \"return add(a-times-four,b-times-three)\")");
+    
+    Evaluator evaluator;
+    Term* result = quick_eval_function(branch, "sub(2,1)");
+    evaluator.evaluate(result);
+    evaluator.runUntilFinished();
+
+    std::cout << result->toString() << std::endl;
+
+    test_assert(as_int(result) == 11);
 }
 
 void all_tests()
