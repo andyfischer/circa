bef51c04f769f7035deaa56d20f75df83bf99c6d
Andy Fischer <andy.fischer@gmail.com>
2008-08-24 01:46:12 -0700

    in parser.cpp, each function now takes a reference argument instead of a pointer

--- src/parser.h
+++ src/parser.h
@@ -7,14 +7,14 @@
 namespace circa {
 namespace parser {
 
-ast::StatementList* statementList(token_stream::TokenStream* tokens);
-ast::Statement* statement(token_stream::TokenStream* tokens);
-ast::Expression* infixExpression(token_stream::TokenStream* tokens);
-ast::Expression* atom(token_stream::TokenStream* tokens);
-ast::FunctionCall* functionCall(token_stream::TokenStream* tokens);
-ast::LiteralString* literalString(token_stream::TokenStream* tokens);
-ast::LiteralFloat* literalFloat(token_stream::TokenStream* tokens);
-ast::Identifier* identifier(token_stream::TokenStream* tokens);
+ast::StatementList* statementList(token_stream::TokenStream& tokens);
+ast::Statement* statement(token_stream::TokenStream& tokens);
+ast::Expression* infixExpression(token_stream::TokenStream& tokens);
+ast::Expression* atom(token_stream::TokenStream& tokens);
+ast::FunctionCall* functionCall(token_stream::TokenStream& tokens);
+ast::LiteralString* literalString(token_stream::TokenStream& tokens);
+ast::LiteralFloat* literalFloat(token_stream::TokenStream& tokens);
+ast::Identifier* identifier(token_stream::TokenStream& tokens);
 
 } // namespace parser
 } // namespace circa
