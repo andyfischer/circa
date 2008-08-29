dce099d97aa9b8ce30e63d1433611f7d2aa7de99
Paul Hodge <paul.hodge.email@gmail.com>
2008-08-28 19:44:58 -0700

    Add some tests on containers

--- src/tests/container_tests.cpp
+++ src/tests/container_tests.cpp
@@ -3,8 +3,9 @@
 
 #include "tests/common.h"
 #include "term.h"
-#include "term_set.h"
 #include "term_list.h"
+#include "term_namespace.h"
+#include "term_set.h"
 
 namespace circa {
 namespace container_tests {
@@ -43,6 +44,37 @@ void test_set()
 
     test_assert(!set.contains(term2));
     test_assert(set.count() == 0);
+
+    set.add(term1);
+    test_assert(set.contains(term1));
+    test_assert(set.count() == 1);
+
+    /*TermMap remap;
+    remap[term1] = term2;
+    set.remapPointers(remap);
+    test_assert(set.contains(term2));
+    test_assert(set.count() == 1);*/
+}
+
+void test_namespace()
+{
+    TermNamespace nspace;
+    Term *term = new Term();
+
+    nspace.bind(term, "a");
+    test_assert(nspace.contains("a"));
+    test_assert(nspace["a"] == term);
+
+    Term *term2 = new Term();
+    TermMap remap;
+    remap[term] = term2;
+    nspace.remapPointers(remap);
+    test_assert(nspace["a"] == term2);
+
+    test_assert(nspace.contains("a"));
+    remap[term2] = NULL;
+    nspace.remapPointers(remap);
+    test_assert(!nspace.contains("a"));
 }
 
 } // namespace container_tests
@@ -50,6 +82,7 @@ void test_set()
 void register_container_tests()
 {
     REGISTER_TEST_CASE(container_tests::test_set);
+    REGISTER_TEST_CASE(container_tests::test_namespace);
 }
 
 } // namespace circa
