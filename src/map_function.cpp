e559eb51bf1dd111f6ac2df39fd0540c59459e7d
Paul Hodge <paul.hodge.email@gmail.com>
2008-08-31 09:45:56 -0700

    WIP on map function

--- src/map_function.cpp
+++ src/map_function.cpp
@@ -3,18 +3,41 @@
 
 #include "bootstrapping.h"
 #include "builtins.h"
+#include "cpp_interface.h"
 #include "map_function.h"
 
 namespace circa {
 
-void map__create__evaluate(Term* term)
+void map__evaluate(Term* term);
+
+struct MapFunctionState
+{
+    Branch branch;
+    TermMap map;
+};
+
+Term* MAP_FUNCTION_STATE_TYPE = NULL;
+
+void map_create__evaluate(Term* term)
+{
+    Term* inputType = term->inputs[0];
+    Term* outputType = term->inputs[1];
+
+    Function *function = as_function(term);
+    function->inputTypes = TermList(inputType);
+    function->outputType = outputType;
+    function->name = "map<" + as_type(inputType)->name + "," + as_type(outputType)->name + ">";
+}
+
+void map__evaluate(Term* term)
 {
 
 }
 
 void initialize_map_function(Branch* kernel)
 {
-    quick_create_function(kernel, "map", map__create__evaluate,
+    MAP_FUNCTION_STATE_TYPE = quick_create_cpp_type<MapFunctionState>(kernel, "MapState");
+    quick_create_function(kernel, "map", map_create__evaluate,
         TermList(TYPE_TYPE, TYPE_TYPE), FUNCTION_TYPE);
 }
 
