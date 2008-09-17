166c1a255b92c7ea1eb82166331f730353366c87
Paul Hodge <paul.hodge.email@gmail.com>
2008-09-17 10:41:35 -0700

    Added implementation for CompoundType, plus a test

--- src/builtins.h
+++ src/builtins.h
@@ -20,7 +20,6 @@ extern Term* TYPE_TYPE;
 extern Term* FUNCTION_TYPE;
 extern Term* CODEUNIT_TYPE;
 extern Term* SUBROUTINE_TYPE;
-extern Term* STRUCT_DEFINITION_TYPE;
 extern Term* COMPOUND_TYPE_TYPE;
 extern Term* BRANCH_TYPE;
 extern Term* ANY_TYPE;
