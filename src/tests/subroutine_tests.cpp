323bd07839bf87719e899fd0838694fed7472982
Andy Fischer <andy.fischer@gmail.com>
2008-09-30 19:11:40 -0700

    Add some (broken) test cases

--- src/tests/subroutine_tests.cpp
+++ src/tests/subroutine_tests.cpp
@@ -57,11 +57,37 @@ void test_using_evaluator()
     test_assert(as_float(result) == 11);
 }
 
+void create()
+{
+    Branch branch;
+
+    Term* sub = eval_statement(branch, "sub = subroutine-create('my-sub, list(int), string)");
+
+    test_assert(is_subroutine(sub));
+    test_assert(as_function(sub).name == "my-sub");
+    test_assert(as_function(sub).inputTypes[0] == INT_TYPE);
+    test_assert(as_function(sub).inputTypes.count() == 1);
+    test_assert(as_function(sub).outputType == STRING_TYPE);
+
+    // name input
+    sub = eval_statement(branch, "function-name-input(@sub, 0, 'apple)");
+}
+
+void using_apply()
+{
+    Branch branch;
+
+    eval_statement(branch, "sub = subroutine-create('s, list(int), int)");
+    eval_statement(branch, "function-name-input(@sub, 0, 'x)");
+    eval_statement(branch, "subroutine-apply(@sub, \"return add(mult(x,2)5)\"");
+}
+
 } // namespace subroutine_tests
 
 void register_subroutine_tests()
 {
-    // REGISTER_TEST_CASE(subroutine_tests::test_using_evaluator);
+    //REGISTER_TEST_CASE(subroutine_tests::create);
+    REGISTER_TEST_CASE(subroutine_tests::using_apply);
 }
 
 } // namespace circa
