b52c88faf82c23aadc6d50b120b7a8158de5c171
Paul Hodge <paul.hodge.email@gmail.com>
2008-10-09 18:19:14 -0700

    add Dictionary and Term.properties

---
+++ src/dictionary.h
@@ -0,0 +1,43 @@
+// Copyright 2008 Paul Hodge
+
+#ifndef CIRCA_DICTIONARY_INCLUDED
+#define CIRCA_DICTIONARY_INCLUDED
+
+#include "ref_list.h"
+#include "branch.h"
+
+namespace circa {
+
+struct Dictionary
+{
+private:
+    typedef std::map<std::string, Term*> RefDictionary;
+    RefDictionary _dict;
+    Branch _branch;
+public:
+
+    // Default constructor
+    Dictionary() {}
+
+    Term* operator[] (std::string const& name) const { return this->get(name); }
+    Term* get(std::string const& name) const {
+        RefDictionary::const_iterator it = _dict.find(name);
+
+        if (it == _dict.end())
+            return NULL;
+
+        return it->second;
+    }
+
+    Term* addSlot(std::string const& name, Term* type);
+
+    void clear()
+    {
+        _dict.clear();
+        _branch.clear();
+    }
+};
+
+} // namespace circa
+
+#endif
