3216c7236430df7c77ed909225d998ff92bf68f1
Paul Hodge <paul.hodge.email@gmail.com>
2008-10-28 18:16:23 -0700

    Preliminary work on cpp code generation

---
+++ src/codegen.h
@@ -0,0 +1,17 @@
+// Copyright 2008 Paul Hodge
+
+#ifndef CIRCA_CODEGEN_INCLUDED
+#define CIRCA_CODEGEN_INCLUDED
+
+#include "common_headers.h"
+
+#include "output_stream.h"
+
+namespace circa {
+
+void generate_cpp_code(Term* term, OutputStream& output);
+
+
+}
+
+#endif
