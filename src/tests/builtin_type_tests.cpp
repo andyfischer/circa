8e08b54f4289687194634765d55c2b7ad932a970
Paul Hodge <paul.hodge.email@gmail.com>
2008-10-30 17:36:08 -0700

    Initial add of Dictionary type

---
+++ src/tests/builtin_type_tests.cpp
@@ -0,0 +1,31 @@
+// Copyright 2008 Paul Hodge
+
+#include "common_headers.h"
+
+#include <tests/common.h>
+#include "cpp_interface.h"
+#include "builtins.h"
+#include "branch.h"
+#include "parser.h"
+
+namespace circa {
+namespace builtin_type_tests {
+
+void test_dictionary()
+{
+    Branch branch;
+    Term *d = eval_statement(branch, "d = Dictionary()");
+    test_assert(d != NULL);
+    as<Dictionary>(d);
+
+    // todo: add more here
+}
+
+} // namespace builtin_type_tests
+
+void register_builtin_type_tests()
+{
+    REGISTER_TEST_CASE(builtin_type_tests::test_dictionary);
+}
+
+} // namespace circa
