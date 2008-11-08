2929d88748a45f59d0f3491123bfce3a2e8bcd12
Andy Fischer <andy.fischer@gmail.com>
2008-11-07 18:22:17 -0800

    Add code-generation script

---
+++ codegen/generate.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os, string
+
+TEMPLATE = string.Template("""
+// Copyright 2008 Andrew Fischer
+
+namespace ${name}_function {
+
+    void evaluate(Term* caller)
+    {
+        $evaluate
+    }
+
+    $feedback_propogate_defn
+    void setup(Branch& kernel)
+    {
+        Term* main_func = import_c_function(kernel, evaluate,
+                $header);
+        as_function(main_func).pureFunction = $is_pure;
+
+        $feedback_propogate_setup
+    }
+}
+""")
+
+FEEDBACK_PROPOGATE_DEFN_TEMPLATE = string.Template("""
+    void feedback_propogate(Term* caller)
+    {
+        $feedback_propogate
+    }
+""")
+
+FEEDBACK_PROPOGATE_SETUP_TEMPLATE = string.Template("""
+        Term* fp_func = import_c_function(kernel, feedback_propogate,
+                "function ${name}-feedback-propogate(any,any)");
+        as_function(fp_func).stateType = BRANCH_TYPE;
+        as_function(main_func).feedbackPropogateFunction = fp_func;
+""")
+
+def generate_function(functionName):
+
+    root_path = os.path.join('..','src','builtin_functions')
+    py_source_path = os.path.join(root_path, functionName + '.source.py')
+    output_path = os.path.join(root_path, functionName + '.cpp')
+
+    if not os.path.exists(py_source_path):
+        raise Exception("couldn't find file: "+py_source_path)
+    if not os.path.exists(output_path):
+        raise Exception("couldn't find file: "+output_path)
+
+    configs = {'name':functionName}
+
+    execfile(py_source_path, configs)
+
+    configs['is_pure'] = 'true' if configs['pure'] else 'false'
+
+    if 'feedback_propogate' in configs:
+        configs['feedback_propogate_defn'] = FEEDBACK_PROPOGATE_DEFN_TEMPLATE.substitute(configs)
+        configs['feedback_propogate_setup'] = FEEDBACK_PROPOGATE_SETUP_TEMPLATE.substitute(configs)
+    else:
+        configs['feedback_propogate_defn'] = ""
+        configs['feedback_propogate_setup'] = ""
+
+    result = TEMPLATE.substitute(configs)
+
+    print result
+
+    
+generate_function("add")
