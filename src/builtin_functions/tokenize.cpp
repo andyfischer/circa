0716890875b32f90c86611f8d114294b59c7598a
Andy Fischer <andy.fischer@gmail.com>
2008-11-12 10:21:03 -0800

    Improve code generation; remove extra blank lines

--- src/builtin_functions/tokenize.cpp
+++ src/builtin_functions/tokenize.cpp
@@ -8,20 +8,18 @@ namespace tokenize_function {
 
     void evaluate(Term* caller)
     {
-        
         std::string& input = as_string(caller->inputs[0]);
-
+        
         as<token_stream::TokenStream>(caller).reset(input);
-
     }
 
-    
+
     void setup(Branch& kernel)
     {
         Term* main_func = import_c_function(kernel, evaluate,
                 "function tokenize(string) -> TokenStream");
         as_function(main_func).pureFunction = true;
 
-        
+
     }
 }
