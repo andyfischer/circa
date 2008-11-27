64e8a584879243584ab51ab84ae09d7d16dceba7
Andy Fischer <andy.fischer@gmail.com>
2008-11-27 12:57:47 -0800

    Missing include guard

--- src/builtin_types.h
+++ src/builtin_types.h
@@ -1,7 +1,12 @@
 // Copyright 2008 Andrew Fischer
 
+#ifndef CIRCA_BUILTIN_TYPES_INCLUDED
+#define CIRCA_BUILTIN_TYPES_INCLUDED
+
 namespace circa {
 
 void initialize_builtin_types(Branch* kernel);
 
 } // namespace circa
+
+#endif
