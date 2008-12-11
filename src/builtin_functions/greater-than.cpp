c3a2db57bd73ebf3bce4bdfcc782fb6407c712b9
Paul Hodge <paul.hodge.email@gmail.com>
2008-12-11 10:19:44 -0800

    Add 'greater-than' function. Enable 'if.ca' output test. This test mostly passes, but fails at rebinding

---
+++ src/builtin_functions/greater-than.cpp
@@ -0,0 +1,31 @@
+// Copyright 2008 Paul Hodge
+
+#include "circa.h"
+
+namespace circa {
+namespace greater_than_function {
+
+    void evaluate(Term* caller)
+    {
+        Term* input0 = caller->input(0);
+        Term* input1 = caller->input(1);
+        assert(input0->type == input1->type);
+
+        Type &type = as_type(input0->type);
+
+        if (type.lessThan == NULL) {
+            error_occured(caller, "greaterThan not defined");
+            return;
+        }
+
+        as_bool(caller) = !type.lessThan(input0, input1);
+    }
+
+    void setup(Branch& kernel)
+    {
+        Term* main_func = import_c_function(kernel, evaluate,
+                "function greater-than(any,any) -> bool");
+        as_function(main_func).pureFunction = true;
+    }
+}
+}
--- src/setup_builtin_functions.cpp
+++ src/setup_builtin_functions.cpp
@@ -16,6 +16,7 @@ namespace cos_function { void setup(Branch& kernel); }
 namespace div_function { void setup(Branch& kernel); }
 namespace equals_function { void setup(Branch& kernel); }
 namespace evaluate_file_function { void setup(Branch& kernel); }
+namespace greater_than_function { void setup(Branch& kernel); }
 namespace if_expr_function { void setup(Branch& kernel); }
 namespace if_statement_function { void setup(Branch& kernel); }
 namespace less_than_function { void setup(Branch& kernel); }
@@ -49,6 +50,7 @@ void setup_builtin_functions(Branch& kernel)
     div_function::setup(kernel);
     equals_function::setup(kernel);
     evaluate_file_function::setup(kernel);
+    greater_than_function::setup(kernel);
     if_expr_function::setup(kernel);
     if_statement_function::setup(kernel);
     less_than_function::setup(kernel);
