627a2d7cb6e641976d1eee11a6afb8489c606706
Paul Hodge <paul.hodge.email@gmail.com>
2008-12-24 23:52:46 -0700

    Start module wrappers.cpp

---
+++ src/wrappers.cpp
@@ -0,0 +1,17 @@
+// Copyright 2008 Paul Hodge
+
+#include "ast.h"
+#include "branch.h"
+#include "cpp_interface.h"
+#include "values.h"
+
+namespace circa {
+
+std::string getInfixFunctionName(std::string infix)
+{
+    Branch workspace;
+    string_value(workspace, infix, "infix");
+    return eval_as<std::string>(workspace, "get-infix-function-name(infix)");
+}
+    
+} // namespace circa
