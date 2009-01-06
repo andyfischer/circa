a3b2893068534fc17a3916fadc1b1f228fd4a320
Andy Fischer <andy.fischer@gmail.com>
2009-01-06 09:52:34 -0800

    Coerce from ints to floats

---
+++ src/builtin_functions/int-to-float.cpp
@@ -0,0 +1,22 @@
+// Copyright 2008 Andrew Fischer
+
+#include "branch.h"
+#include "circa.h"
+#include "introspection.h"
+
+namespace circa {
+namespace int_to_float_function {
+
+    void evaluate(Term* caller)
+    {
+        as_float(caller) = as_int(caller->input(0));
+    }
+
+    void setup(Branch& kernel)
+    {
+        Term* main_func = import_c_function(kernel, evaluate,
+                "function int-to-float(int) -> float");
+        as_function(main_func).pureFunction = true;
+    }
+}
+}
