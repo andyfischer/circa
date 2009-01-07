dff62527c8a11fcc631e42064de6316185e7f893
Andy Fischer <andy.fischer@gmail.com>
2009-01-06 18:53:43 -0800

    Bring back user tracking

--- src/runtime.h
+++ src/runtime.h
@@ -10,29 +10,37 @@ namespace circa {
 void evaluate_term(Term* term);
 void evaluate_branch(Branch& branch, Term* errorListener=NULL);
 
+// Puts 'errorTerm' into an error state, with the given message.
 void error_occured(Term* errorTerm, std::string const& message);
 
+// Create a term with the given function and inputs.
+// This function does not do any magic; it (attempts) to use your exact function and 
+// exact inputs. You may want to instead use apply_function(), which does some
+// helpful magic.
 Term* create_term(Branch* branch, Term* function, ReferenceList const& inputs);
-void set_inputs(Term* term, ReferenceList const& inputs);
 void set_input(Term* term, int index, Term* input);
+
+// Check the function and inputs of 'user', returns whether they are actually
+// using 'usee'.
+bool is_actually_using(Term* user, Term* usee);
 
 // Examine 'function' and 'inputs' and returns a result term. A few things
 // may happen here:
 //  1. We might re-use an existing term
-//  2. 'function' might be a type (create an empty instance)
+//  2. 'function' might be a type, in which case we create a value of this type.
 //  3. We might specialize an overloaded function
-//  4. Something else that is possibly implemented in the future?
+//  4. We might coerce inputs to a different type.
 Term* apply_function(Branch& branch, Term* function, ReferenceList const& inputs);
 
-// Look up the given function in branch, and then call the above apply_function
+// Find the named function in this branch, and then call the above apply_function.
 Term* apply_function(Branch& branch,
                      std::string const& functionName, 
                      ReferenceList const& inputs);
 
-// Perform 'apply_function' and then evaluate the result
+// Perform 'apply_function' and then evaluate the result.
 Term* eval_function(Branch& branch, Term* function, ReferenceList const& inputs);
 
-// Look up the given function in branch, and then do the above 'eval_function'
+// Find the named function in this branch, and then do the above 'eval_function'.
 Term* eval_function(Branch& branch,
                     std::string const& functionName,
                     ReferenceList const& inputs);
@@ -42,12 +50,6 @@ void change_function(Term* term, Term* new_function);
 void remap_pointers(Term* term, ReferenceMap const& map);
 void remap_pointers(Term* term, Term* original, Term* replacement);
 void visit_pointers(Term* term, PointerVisitor &visitor);
-
-// Assign the pointer 'pointer' to 'value', where 'owner' is the owner.
-// This updates the 'users' field of value (if value is non-NULL),
-// and it also updates 'users' on the previous value of 'pointer' (if
-// that was non-NULL). 'owner' must not be NULL.
-void assign_pointer(Term*& pointer, Term* value, Term* owner);
 
 } // namespace circa
 
