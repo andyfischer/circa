225d01ccb6a29abe9a3658165fb7a44680e1aa4e
Paul Hodge <paul.hodge.email@gmail.com>
2009-01-13 18:58:38 -0800

    Add Float to primitive_wrappers, add temporary fix for how mutability is accessed

--- src/primitive_wrappers.h
+++ src/primitive_wrappers.h
@@ -41,6 +41,37 @@ public:
     }
 };
 
+class Float {
+private:
+    Term* _term;
+    bool _owned;
+public:
+    Float() {
+        _term = create_value(NULL, FLOAT_TYPE);
+        _owned = true;
+    }
+    Float(Branch &branch, std::string const& name="") {
+        _term = create_value(&branch, FLOAT_TYPE, NULL, name);
+        _owned = false;
+    }   
+    Float(Term* term) {
+        _term = term;
+        _owned = false;
+    }
+    ~Float() {
+        if (_owned)
+            delete _term;
+    }
+    Float& operator=(int value)
+    {
+        as_float(_term) = value;
+        return *this;
+    }
+    operator int() const
+    {
+        return as_float(_term);
+    }
+};
 }
 
 #endif
