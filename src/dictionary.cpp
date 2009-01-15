4d1dd37ea7302390d599f1ba762a54374cbc12f0
Paul Hodge <paul.hodge.email@gmail.com>
2009-01-15 09:14:56 -0800

    Refactor. Dictionary no longer keeps an inner branch

--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -4,17 +4,36 @@
 
 #include "dictionary.h"
 #include "runtime.h"
+#include "term.h"
 #include "values.h"
 
 namespace circa {
+
+Term* Dictionary::get(std::string const& name) const
+{
+    RefDictionary::const_iterator it = _dict.find(name);
+
+    if (it == _dict.end())
+        return NULL;
+
+    return it->second;
+}
 
 Term* Dictionary::addSlot(std::string const& name, Term* type)
 {
     assert(get(name) == NULL);
 
-    Term* newTerm = create_value(&_branch, type);
+    Term* newTerm = create_value(NULL, type);
     _dict[name] = newTerm;
     return newTerm;
+}
+
+void Dictionary::clear()
+{
+    RefDictionary::iterator it;
+    for (it = _dict.begin(); it != _dict.end(); ++it)
+        delete it->second;
+    _dict.clear();
 }
 
 } // namespace circa
