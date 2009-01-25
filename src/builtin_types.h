844adf8744b473f3592adffb9beb046fee49bdf2
Paul Hodge <paul.hodge.email@gmail.com>
2009-01-24 21:42:23 -0800

    Move CompoundValue to a separate file

--- src/builtin_types.h
+++ src/builtin_types.h
@@ -9,31 +9,7 @@
 
 namespace circa {
 
-const int COMPOUND_TYPE_SIGNATURE = 0x43214321;
-
-struct CompoundValue
-{
-    int signature;
-    Branch branch;
-    ReferenceList fields;
-
-    // Member functions
-    CompoundValue() : signature(COMPOUND_TYPE_SIGNATURE) {}
-
-    Term* appendSlot(Term* type);
-
-    // Static functions
-    static void* alloc(Term* typeTerm);
-    static void dealloc(void* data);
-    static void create_compound_type(Term* term);
-    static void append_field(Term* term);
-};
-
 void initialize_builtin_types(Branch& kernel);
-
-Term* get_field(Term *term, std::string const& fieldName);
-Term* get_field(Term *term, int index);
-CompoundValue& as_compound_value(Term *term);
 
 } // namespace circa
 
