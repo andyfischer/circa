b628de81683557520c6ae55e8e2d3389f7ae6fa4
Andy Fischer <andy.fischer@gmail.com>
2009-01-25 14:50:27 -0800

    Add CompoundValuePointerIterator with test

---
+++ src/tests/compound_value_tests.cpp
@@ -0,0 +1,38 @@
+// Copyright 2008 Andrew Fischer
+
+#include "common_headers.h"
+
+#include "circa.h"
+#include "testing.h"
+
+namespace circa {
+namespace compound_value_tests {
+
+void test_iterator()
+{
+    CompoundValue value;
+
+    Term* ref = value.appendSlot(REFERENCE_TYPE);
+    
+    as_ref(ref) = INT_TYPE;
+
+    PointerIterator* it = start_compound_value_pointer_iterator(&value);
+
+    test_assert(it->current()->function == VALUE_FUNCTION_GENERATOR);
+    it->advance();
+    test_assert(it->current() == REFERENCE_TYPE);
+    it->advance();
+    test_assert(it->current() == INT_TYPE);
+    it->advance();
+    test_assert(it->finished());
+
+    delete it;
+}
+
+void register_tests()
+{
+    REGISTER_TEST_CASE(compound_value_tests::test_iterator);
+}
+
+} // namespace compound_value_tests
+} // namespace circa
