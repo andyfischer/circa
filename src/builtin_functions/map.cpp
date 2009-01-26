6af122ca7c51c91ea16b86e8886e290f8254ba7d
Andy Fischer <andy.fischer@gmail.com>
2009-01-25 18:31:11 -0800

    Move the Map type to a separate file

--- src/builtin_functions/map.cpp
+++ src/builtin_functions/map.cpp
@@ -2,63 +2,20 @@
 
 #include "circa.h"
 
+#include "builtin_types/map.hpp"
+
 namespace circa {
 namespace map_function {
 
-    static Term* STATE_TYPE = NULL;
     static Term* FEEDBACK_PROPOGATE = NULL;
 
     namespace specialized_map_function {
-        struct Pair {
-            Term* key;
-            Term* value;
-
-            Pair() : key(NULL), value(NULL) {}
-        };
-
-        struct State {
-            // TODO: more efficient data structure
-            std::vector<Pair> pairs;
-
-            Pair* findPair(Term* key) {
-                std::vector<Pair>::iterator it;
-                for (it = pairs.begin(); it != pairs.end(); ++it) {
-                    if (values_equal(key, it->key))
-                        return &*it;
-                }
-                return NULL;
-            }
-
-            Term* find(Term* key) {
-                Pair *pair = findPair(key);
-                if (pair == NULL) return NULL;
-                return pair->value;
-            }
-
-            void assign(Term* key, Term* value) {
-                Pair *existing = findPair(key);
-
-                Term* duplicatedValue = create_value(NULL, value->type);
-                duplicate_value(value, duplicatedValue);
-
-                if (existing != NULL) {
-                    existing->value = duplicatedValue;
-                } else {
-                    Term* duplicatedKey = create_value(NULL, key->type);
-                    duplicate_value(key, duplicatedKey);
-                    Pair newPair;
-                    newPair.key = duplicatedKey;
-                    newPair.value = duplicatedValue;
-                    pairs.push_back(newPair);
-                }
-            }
-        };
 
         void evaluate(Term* caller)
         {
             Term* mapFunction = caller->function;
             assert(mapFunction->state != NULL);
-            State &state = as<State>(mapFunction->state);
+            Map &state = as<Map>(mapFunction->state);
             Term* value = state.find(caller->input(0));
             if (value == NULL) {
                 error_occured(caller, "key not found");
@@ -76,10 +33,8 @@ namespace map_function {
             Term* target = caller->input(0);
 
             assert(target->function->state != NULL);
-            assert(target->function->state->type == STATE_TYPE);
 
-            specialized_map_function::State &mapState =
-                as<specialized_map_function::State>(target->function->state);
+            Map &mapState = as<Map>(target->function->state);
             Term* key = target->input(0);
             Term* desired = caller->input(1);
 
@@ -109,9 +64,7 @@ namespace map_function {
                 "function map(Type,Type) -> Function");
         as_function(main_func).pureFunction = true;
 
-        STATE_TYPE = import_type<specialized_map_function::State>(kernel);
-
-        as_function(main_func).stateType = STATE_TYPE;
+        as_function(main_func).stateType = kernel["Map"];
 
         FEEDBACK_PROPOGATE = import_function(kernel, feedback_propogate::evaluate,
                 "function map-feedback(any, any)");
