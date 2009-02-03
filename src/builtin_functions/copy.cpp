4e4c955dbdc9e6c1f3c2eae60b767bfc97445197
Andy Fischer <andy.fischer@gmail.com>
2009-02-02 19:01:22 -0800

    Add copy function

---
+++ src/builtin_functions/copy.cpp
@@ -0,0 +1,24 @@
+// Copyright 2008 Andrew Fischer
+
+#include "circa.h"
+
+namespace circa {
+namespace copy_function {
+
+    void evaluate(Term* caller)
+    {
+        // remove this when type parametrezation is in
+        if (caller->input(0)->type != caller->type)
+            specialize_type(caller, caller->input(0)->type);
+
+        copy_value(caller->input(0), caller);
+    }
+
+    void setup(Branch& kernel)
+    {
+        Term* main_func = import_function(kernel, evaluate,
+                "function copy(any) -> any");
+        as_function(main_func).pureFunction = true;
+    }
+}
+}
