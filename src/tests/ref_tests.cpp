d446a774dc1ada666a9fb5fbfe2630cacfe246ee
Paul Hodge <paul.hodge.email@gmail.com>
2009-02-16 16:07:04 -0800

    More tests in ref_tests

--- src/tests/ref_tests.cpp
+++ src/tests/ref_tests.cpp
@@ -28,17 +28,52 @@ void test_basic()
 
     test_assert(ref._target == NULL);
     test_assert(t->refs.size() == 0);
+
+    delete t;
 }
 
 void test_copy()
 {
+    Ref ref(NULL);
 
+    Term* t = new Term();
+
+    ref = t;
+
+    Ref copy(ref);
+
+    test_assert(copy._target == t);
+    test_assert(t->refs.size() == 2);
+    test_assert(t->refs[0] == &ref);
+    test_assert(t->refs[1] == &copy);
+
+    Ref another_copy(NULL);
+    another_copy = copy;
+    test_assert(another_copy._target == t);
+    test_assert(t->refs.size() == 3);
+    test_assert(t->refs[2] == &another_copy);
+
+    delete t;
+}
+
+void test_destroy()
+{
+    Term* t = new Term();
+
+    {
+        Ref ref(NULL);
+        ref = t;
+        test_assert(t->refs.size() == 1);
+    }
+
+    test_assert(t->refs.size() == 0);
 }
 
 void register_tests()
 {
     REGISTER_TEST_CASE(ref_tests::test_basic);
     REGISTER_TEST_CASE(ref_tests::test_copy);
+    REGISTER_TEST_CASE(ref_tests::test_destroy);
 }
 } // namespace ref_tests
 
