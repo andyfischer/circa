92d7c16ed58e98c1a1862c34f24496105b040d31
Paul Hodge <paul.hodge.email@gmail.com>
2009-02-18 23:00:32 -0800

    Move delete_term into references.cpp

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -51,43 +51,6 @@ Term* create_term(Branch* branch, Term* function, RefList const& inputs)
     return term;
 }
 
-void delete_term(Term* term)
-{
-    if (term->state != NULL)
-        delete_term(term->state);
-    term->state = NULL;
-
-    dealloc_value(term);
-
-    // Clear references
-    std::vector<Ref*>::iterator it;
-    for (it = term->refs.begin(); it != term->refs.end(); ++it) {
-        (*it)->_target = NULL;
-    }
-
-    term->refs.clear();
-    
-
-#if TRACK_USERS
-    // Remove us from 'user' lists
-    for (unsigned int i=0; i < term->inputs.count(); i++) {
-        Term* user = term->inputs[i];
-        if (user == NULL)
-            continue;
-        assert_good_pointer(user);
-        user->users.remove(term);
-    }
-#endif
-
-#if DEBUG_CHECK_FOR_BAD_POINTERS
-    DEBUG_GOOD_POINTER_SET.erase(term);
-#endif
-
-#if !DEBUG_NEVER_DELETE_TERMS
-    delete term;
-#endif
-}
-
 bool check_valid_type(Function &func, int index, Term* term)
 {
     Term* expectedType = func.inputTypes[index];
