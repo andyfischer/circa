f507cc34940487d30c08c387785f56c60be7c39c
Andy Fischer <andy.fischer@gmail.com>
2009-02-20 19:51:31 -0800

    Change style in cgame, use Ref in primitive wrappers

--- src/opt/PrimitiveWrappers.hpp
+++ src/opt/PrimitiveWrappers.hpp
@@ -3,41 +3,30 @@
 #ifndef CIRCA_PRIMITIVE_WRAPPERS_INCLUDED
 #define CIRCA_PRIMITIVE_WRAPPERS_INCLUDED
 
-// This file should not be included by Circa code. It's optional.
-
 #include "circa.h"
 
 namespace circa {
 
 class Int {
 private:
-    Term* _term;
-    bool _owned;
+    Ref _term;
 public:
     Int() {
         _term = create_value(NULL, INT_TYPE);
-        _owned = true;
     }
     Int(Branch &branch, std::string const& name="") {
         if (name != "" && branch.containsName(name)) {
             _term = branch.findFirstBinding(name);
 
             assert(_term->type == INT_TYPE);
-
-            _owned = false;
         } else {
             _term = create_value(&branch, INT_TYPE, name);
-            _owned = false;
         }
     }   
     Int(Term* term) {
         _term = term;
-        _owned = false;
     }
-    ~Int() {
-        if (_owned)
-            delete _term;
-    }
+    ~Int() { }
     Int& operator=(int value)
     {
         as_int(_term) = value;
@@ -51,33 +40,24 @@ public:
 
 class Float {
 private:
-    Term* _term;
-    bool _owned;
+    Ref _term;
 public:
     Float() {
         _term = create_value(NULL, FLOAT_TYPE);
-        _owned = true;
     }
     Float(Branch &branch, std::string const& name="") {
         if (name != "" && branch.containsName(name)) {
             _term = branch.findFirstBinding(name);
 
             assert(_term->type == FLOAT_TYPE);
-
-            _owned = false;
         } else {
             _term = create_value(&branch, FLOAT_TYPE, name);
-            _owned = false;
         }
     }   
     Float(Term* term) {
         _term = term;
-        _owned = false;
     }
-    ~Float() {
-        if (_owned)
-            delete _term;
-    }
+    ~Float() { }
     Float& operator=(float value)
     {
         as_float(_term) = value;
@@ -91,33 +71,24 @@ public:
 
 class Bool {
 private:
-    Term* _term;
-    bool _owned;
+    Ref _term;
 public:
     Bool() {
         _term = create_value(NULL, BOOL_TYPE);
-        _owned = true;
     }
     Bool(Branch &branch, std::string const& name="") {
         if (name != "" && branch.containsName(name)) {
             _term = branch.findFirstBinding(name);
 
             assert(_term->type == BOOL_TYPE);
-
-            _owned = false;
         } else {
             _term = create_value(&branch, BOOL_TYPE, name);
-            _owned = false;
         }
     }   
     Bool(Term* term) {
         _term = term;
-        _owned = false;
     }
-    ~Bool() {
-        if (_owned)
-            delete _term;
-    }
+    ~Bool() { }
     Bool& operator=(bool value)
     {
         as_bool(_term) = value;
