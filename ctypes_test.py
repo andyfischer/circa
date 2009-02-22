a6c18a1d4a6aac7a84cb4035153214a1bb455ab2
Paul Hodge <paul.hodge.email@gmail.com>
2009-02-22 10:54:55 -0800

    More work on python shell

--- ctypes_test.py
+++ ctypes_test.py
@@ -1,4 +1,84 @@
 
+import readline
+import cmd, sys
 from ctypes import *
+
 libcirca = cdll.LoadLibrary("lib/libcirca.dylib")
-libcirca.evaluate_file(libcirca.new_branch(), "space.ca")
+libcirca.print_raw_branch.restype = c_char_p
+libcirca.print_raw_term.restype = c_char_p
+libcirca.term_num_inputs.restype = c_int
+libcirca.initialize()
+
+def error(s):
+    print "error:",s
+
+def print_term(term):
+    return str(libcirca.print_raw_term(term))
+
+class Branch(object):
+    def __init__(self, ptr=None):
+        self.ptr = ptr
+    def find_named(self, name):
+        return libcirca.find_named(self.ptr, name)
+    def print_raw(self):
+        return libcirca.print_raw_branch(self.ptr)
+
+def evaluate_file(filename):
+    ptr = libcirca.new_branch()
+    libcirca.evaluate_file(ptr, filename)
+    return Branch(ptr)
+
+class CommandLine(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "> "
+        self.branch = None
+        self.termTargetStack = []
+
+    def termTarget(self):
+        if self.termTargetStack is []: return None
+        else: return self.termTargetStack[-1]
+
+    def pushTarget(self, term):
+        print "cursor:", print_term(term)
+        self.termTargetStack.append(term)
+
+    def do_open(self, line):
+        self.branch = evaluate_file(line)
+        print "opened:", line
+
+    def do_t(self, line):
+        if line[0] == '#':
+            error("unimp")
+        else:
+            name = line
+            t = self.branch.find_named(name)
+            self.pushTarget(t)
+
+    def do_input(self, line):
+        i = int(line)
+        t = libcirca.term_get_input(self.termTarget(), i)
+        self.pushTarget(t)
+
+    def do_show(self, line):
+        if self.branch is None:
+            print "(no branch loaded)"
+        else:
+            print self.branch.print_raw()
+
+    def do_down(self, line):
+        term = self.branch.find_named(line)
+
+    def do_num_inputs(self, line):
+        print libcirca.term_num_inputs(self.termTarget())
+
+
+cl = CommandLine()
+
+if len(sys.argv) > 1:
+    cl.do_open(sys.argv[1])
+
+try:
+    cl.cmdloop()
+except KeyboardInterrupt:
+    print ""
--- src/extern_c.cpp
+++ src/extern_c.cpp
@@ -2,18 +2,55 @@
 
 #include "circa.h"
 
-using namespace circa;
 
 extern "C" {
 
-Branch* new_branch()
+void initialize()
 {
-    return new Branch();
+    circa::initialize();
 }
 
-void evaluate_file(Branch* branch, const char* filename)
+circa::Branch* new_branch()
 {
-    evaluate_file(*branch, filename);
+    return new circa::Branch();
+}
+
+void evaluate_file(circa::Branch* branch, const char* filename)
+{
+    circa::evaluate_file(*branch, filename);
+}
+
+const char* print_raw_term(circa::Term* term)
+{
+    static std::string result;
+    std::stringstream sstream;
+    circa::print_raw_term(term, sstream);
+    result = sstream.str();
+    return result.c_str();
+}
+
+const char* print_raw_branch(circa::Branch* branch)
+{
+    static std::string result;
+    std::stringstream sstream;
+    circa::print_raw_branch(*branch, sstream);
+    result = sstream.str();
+    return result.c_str();
+}
+
+int term_num_inputs(circa::Term* term)
+{
+    return term->inputs.count();
+}
+
+circa::Term* term_get_input(circa::Term* term, int i)
+{
+    return term->inputs[i];
+}
+
+circa::Term* find_named(circa::Branch* branch, const char* name)
+{
+    return circa::find_named(branch, name);
 }
 
 }
