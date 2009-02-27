74920d60412d7edcabe4f51471fd3963014c7e58
Paul Hodge <paul.hodge.email@gmail.com>
2009-02-27 10:08:20 -0800

    Minor fix in migrate_terms

--- src/builtin_functions/stateful-value.cpp
+++ src/builtin_functions/stateful-value.cpp
@@ -11,17 +11,21 @@ namespace stateful_value_function {
         Term* bottom = caller->owningBranch->getNamed(caller->name);
 
         if (bottom == caller) {
+            //std::cout << "bottom is same" << std::endl;
             return;
         }
 
         if (!caller->hasValue()) {
+            std::cout << "caller has no value" << std::endl;
             return;
         }
 
         // Temp, ignore terms with wrong type. This check should be
         // removed when we have proper type specialization.
-        if (bottom->type != caller->type)
+        if (bottom->type != caller->type) {
+            std::cout << "type doesn't match" << std::endl;
             return;
+        }
 
         copy_value(bottom, caller);
     }
