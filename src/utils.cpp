984d213f84c882d99c77a7314e59092cddb8328b
Paul Hodge <paul.hodge.email@gmail.com>
2009-03-09 09:14:04 -0700

    Refactor, get rid of wrappers.cpp and introduce utils.cpp

---
+++ src/utils.cpp
@@ -0,0 +1,24 @@
+// Copyright 2008 Paul Hodge
+
+#include "circa.h"
+
+namespace circa {
+
+std::string read_text_file(std::string const& filename)
+{
+    std::ifstream file;
+    file.open(filename.c_str(), std::ios::in);
+    std::stringstream contents;
+    std::string line;
+    bool firstLine = true;
+    while (std::getline(file, line)) {
+        if (!firstLine)
+            contents << "\n";
+        contents << line;
+        firstLine = false;
+    }
+    file.close();
+    return contents.str();
+}
+    
+} // namespace circa
