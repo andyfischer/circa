e039b8c5caa2d4d4a21701206ce1d234acee6513
Andy Fischer <andy.fischer@gmail.com>
2009-03-24 09:39:44 -0700

    Wip on for loop

--- src/builtin_functions/for.cpp
+++ src/builtin_functions/for.cpp
@@ -5,16 +5,12 @@
 namespace circa {
 namespace for_function {
 
-    std::string iterated_variable_name()
-    {
-        return get_name_for_attribute("iter");
-    }
-
     void evaluate(Term* caller)
     {
         Branch& inner = as_branch(caller);
         Branch& series = as_branch(caller->input(0));
 
+        /*
         std::string iterator_name = as_string(inner[iterated_variable_name()]);
         Term* iterator = inner[iterator_name];
         assert(iterator != NULL);
@@ -23,13 +19,14 @@ namespace for_function {
             copy_value(series[i], iterator);
             evaluate_branch(inner);
         }
+        */
     }
 
     void setup(Branch& kernel)
     {
-        Term* main_func = import_function(kernel, evaluate, "function for(Branch)");
-        as_function(main_func).pureFunction = true;
-        as_function(main_func).stateType = BRANCH_TYPE;
+        FOR_FUNC = import_function(kernel, evaluate, "function for(List)");
+        as_function(FOR_FUNC).pureFunction = true;
+        as_function(FOR_FUNC).stateType = BRANCH_TYPE;
     }
 }
 } // namespace circa
