6f20cd975d29c282144eff9fb3bde8100657fa9a
Andy Fischer <andy.fischer@gmail.com>
2009-03-26 15:29:21 -0700

    Some wip on copying identifiers, some tests which don't work yet

--- src/builtin_functions/copy.cpp
+++ src/builtin_functions/copy.cpp
@@ -14,10 +14,16 @@ namespace copy_function {
         copy_value(caller->input(0), caller);
     }
 
+    Term* specializeType(Term* caller)
+    {
+        return caller->input(0)->type;
+    }
+
     void setup(Branch& kernel)
     {
         COPY_FUNC = import_function(kernel, evaluate, "function copy(any) -> any");
         as_function(COPY_FUNC).pureFunction = true;
+        as_function(COPY_FUNC).specializeType = specializeType;
     }
 }
 }
