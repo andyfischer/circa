8818e6e04770686d21d6167c81726b46bcc8dd8f
Andy Fischer <andy.fischer@gmail.com>
2009-03-25 20:55:11 -0700

    Change syntax of for loop

--- src/tests/for_loop_tests.cpp
+++ src/tests/for_loop_tests.cpp
@@ -20,7 +20,7 @@ void test_simple()
     import_function(branch, spy_function, "function spy(int)");
     gSpyResults.clear();
 
-    branch.compile("for int i : range(5)\nspy(i)\nend");
+    branch.compile("for (int i : range(5))\nspy(i)\nend");
 
     evaluate_branch(branch);
 
