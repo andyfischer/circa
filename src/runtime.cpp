bc07793953d48c70dfcb67ca1b5ed7d12b455dbd
Andy Fischer <andy.fischer@gmail.com>
2009-03-26 19:38:21 -0700

    Rename eval_function -> apply_and_eval

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -133,21 +133,21 @@ void error_occured(Term* errorTerm, std::string const& message)
     errorTerm->pushError(message);
 }
 
-Term* eval_function(Branch* branch, Term* function, RefList const& inputs)
+Term* apply_and_eval(Branch* branch, Term* function, RefList const& inputs)
 {
     Term* result = apply(branch, function, inputs);
     evaluate_term(result);
     return result;
 }
 
-Term* eval_function(Branch* branch, std::string const& functionName,
+Term* apply_and_eval(Branch* branch, std::string const& functionName,
         RefList const &inputs)
 {
     Term* function = find_named(branch,functionName);
     if (function == NULL)
         throw std::runtime_error("function not found: "+functionName);
 
-    return eval_function(branch, function, inputs);
+    return apply_and_eval(branch, function, inputs);
 }
 
 } // namespace circa
