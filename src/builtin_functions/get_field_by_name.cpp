4c2cacf812b16cf89d5cf9454cf3e0d761e90576
Paul Hodge <paul.hodge.email@gmail.com>
2009-04-15 22:57:24 -0700

    print() is now more convenient, we accept 'any' and have variable args

--- src/builtin_functions/get_field_by_name.cpp
+++ src/builtin_functions/get_field_by_name.cpp
@@ -8,7 +8,12 @@ namespace get_field_by_name_function {
     void evaluate(Term* caller)
     {
         std::string name = as_string(caller->state);
-        Term* field = as_branch(caller->input(0))[name];
+        int index = as_type(caller->input(0)->type).findFieldIndex(name);
+        if (index == -1) {
+            error_occured(caller, "field not found: " + name);
+            return;
+        }
+        Term* field = as_branch(caller->input(0))[index];
         assign_value(field, caller);
     }
 
