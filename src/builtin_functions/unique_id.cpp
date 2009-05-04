f5745e2a2f36d429ab71b57cd1ef9e663bc21a19
Andy Fischer <andy.fischer@gmail.com>
2009-05-03 18:42:59 -0700

    Add unique_id() function

---
+++ src/builtin_functions/unique_id.cpp
@@ -0,0 +1,20 @@
+// Copyright 2008 Andrew Fischer
+
+#include <circa.h>
+
+namespace circa {
+namespace unique_id_function {
+
+    void evaluate(Term* caller)
+    {
+        static int nextId = 1;
+        as_int(caller) = nextId++;
+    }
+
+    void setup(Branch& kernel)
+    {
+        Term* main_func = import_function(kernel, evaluate, "unique_id() : int");
+        as_function(main_func).pureFunction = false;
+    }
+}
+}
--- src/for_loops.h
+++ src/for_loops.h
@@ -9,6 +9,7 @@ namespace circa {
 
 Term* get_for_loop_iterator(Term* forTerm);
 Branch& get_for_loop_code(Term* forTerm);
+Branch& get_for_loop_state(Term* forTerm, int index);
 void evaluate_for_loop(Term* forTerm, Term* listTerm);
 
 } // namespace circa
