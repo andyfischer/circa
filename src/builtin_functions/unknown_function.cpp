3c43508213d1df9431dfb5257131f8146788c68c
Andy Fischer <andy.fischer@gmail.com>
2009-05-04 21:18:53 -0700

    Compile-error functions now store their message as a property

--- src/builtin_functions/unknown_function.cpp
+++ src/builtin_functions/unknown_function.cpp
@@ -9,13 +9,12 @@ namespace unknown_function_function {
     void evaluate(Term* caller)
     {
         std::cout << "Warning, calling an unknown function: "
-            << as_string(caller->state) << std::endl;
+            << caller->stringProperty("message") << std::endl;
     }
 
     void setup(Branch& kernel)
     {
         Term* main_func = import_function(kernel, evaluate, "unknown(any...) -> any");
-        as_function(main_func).stateType = STRING_TYPE;
         as_function(main_func).pureFunction = false;
         as_function(main_func).hasSideEffects = true;
 
--- src/builtin_functions/unrecognized_expr.cpp
+++ src/builtin_functions/unrecognized_expr.cpp
@@ -12,7 +12,6 @@ namespace unrecognized_expr_function {
     void setup(Branch& kernel)
     {
         UNRECOGNIZED_EXPRESSION_FUNC = import_function(kernel, evaluate, "unrecognized_expr()");
-        as_function(UNRECOGNIZED_EXPRESSION_FUNC).stateType = STRING_TYPE;
         as_function(UNRECOGNIZED_EXPRESSION_FUNC).pureFunction = false;
         as_function(UNRECOGNIZED_EXPRESSION_FUNC).hasSideEffects = true;
     }
