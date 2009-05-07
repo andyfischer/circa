4f6a710f33f63c8a080d6e11b90199fc143b9fcd
Paul Hodge <paul.hodge.email@gmail.com>
2009-05-06 18:39:35 -0700

    Remove deprecated func stateful_value()

--- src/builtins.h
+++ src/builtins.h
@@ -43,7 +43,6 @@ extern Term* REF_TYPE;
 extern Term* SET_FIELD_FUNC;
 extern Term* SET_FIELD_BY_NAME_FUNC;
 extern Term* SUB_FUNC;
-extern Term* STATEFUL_VALUE_FUNC;
 extern Term* STRING_TYPE;
 extern Term* TYPE_TYPE;
 extern Term* UNKNOWN_FUNCTION;
