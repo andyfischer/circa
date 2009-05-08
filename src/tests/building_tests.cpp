574ac0c55932222e553e17eaa3de36a9000f1e00
Andy Fischer <andy.fischer@gmail.com>
2009-05-08 16:11:16 -0400

    Add rewrite_as_value. Fix bugs in load_state_into_branch and persist_state_from_branch

--- src/tests/building_tests.cpp
+++ src/tests/building_tests.cpp
@@ -42,11 +42,44 @@ void test_create_duplicate()
     test_assert(is_stateful(b));
 }
 
+void test_rewrite_as_value()
+{
+    Branch branch;
+
+    Term* a = create_value(&branch, INT_TYPE);
+    Term* b = apply(&branch, ADD_FUNC, RefList(a,a));
+
+    test_assert(branch[1] == b);
+
+    // rewrite b
+    rewrite_as_value(branch, 1, FLOAT_TYPE);
+
+    test_assert(b->type == FLOAT_TYPE);
+    test_assert(b->function == get_value_function(FLOAT_TYPE));
+    test_assert(is_value(b));
+
+    // add new term
+    rewrite_as_value(branch, 2, INT_TYPE);
+    test_assert(branch.numTerms() == 3);
+    Term* c = branch[2];
+    test_assert(is_value(c));
+    test_assert(c->type == INT_TYPE);
+
+    // add a new term such that we need to create NULLs
+    rewrite_as_value(branch, 5, STRING_TYPE);
+    test_assert(branch.numTerms() == 6);
+    test_assert(branch[3] == NULL);
+    test_assert(branch[4] == NULL);
+    test_assert(is_value(branch[5]));
+    test_assert(branch[5]->type == STRING_TYPE);
+}
+
 void register_tests()
 {
     REGISTER_TEST_CASE(building_tests::test_create_value);
     REGISTER_TEST_CASE(building_tests::test_int_value);
     REGISTER_TEST_CASE(building_tests::test_create_duplicate);
+    REGISTER_TEST_CASE(building_tests::test_rewrite_as_value);
 }
 
 } // namespace building_tests
