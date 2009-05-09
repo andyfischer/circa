84f6632da4de613f18506e00032494925ea67579
Andy Fischer <andy.fischer@gmail.com>
2009-05-08 21:07:47 -0700

    Add input_placeholder() func for subroutines. Wip on subroutine feedback

---
+++ src/builtin_functions/input_placeholder.cpp
@@ -0,0 +1,27 @@
+// Copyright 2008 Andrew Fischer
+
+#include <circa.h>
+
+// This function is used inside subroutines, as a placeholder for an incoming
+// input value.
+
+namespace circa {
+namespace input_placeholder_function {
+
+    void evaluate(Term* caller)
+    {
+    }
+
+    void generateFeedback(Branch& branch, Term* subject, Term* desired)
+    {
+        // Just lay down a feedback() term here, we'll find it in subroutine_feedback
+        apply(&branch, FEEDBACK_FUNC, RefList(subject, desired));
+    }
+
+    void setup(Branch& kernel)
+    {
+        INPUT_PLACEHOLDER_FUNC = import_function(kernel, evaluate, "input_placeholder() : any");
+        as_function(INPUT_PLACEHOLDER_FUNC).generateFeedback = generateFeedback;
+    }
+}
+}
