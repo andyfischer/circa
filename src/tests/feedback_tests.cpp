aa3ab1316eaf3cb4ad8bdbcd1ec22ed18d953943
Andy Fischer <andy.fischer@gmail.com>
2009-05-13 17:26:35 -0400

    Fix feedback for mult()

--- src/tests/feedback_tests.cpp
+++ src/tests/feedback_tests.cpp
@@ -39,25 +39,24 @@ void train_addition2()
     refresh_training_branch(branch);
     evaluate_branch(branch);
 
-    test_equals(as_float(a), 1.5);
-    test_equals(as_float(b), 2.5);
+    test_equals(to_float(a), 1.5);
+    test_equals(to_float(b), 2.5);
 }
 
 void train_mult()
 {
     Branch branch;
-    Term* a = branch.eval("a = 2.0");
-    /*Term* b =*/ branch.eval("b = 3.0");
-    Term* c = branch.eval("c = mult(a, b)");
+    Term* a = branch.eval("a = 1");
+    branch.eval("b = 3");
+    branch.eval("c = mult(a, b)");
+    branch.eval("feedback(c, 6)");
 
     set_trainable(a, true);
-    set_trainable(c, true);
 
-    Branch training;
-    //generate_feedback(training, c, training.eval("9.0"));
-    evaluate_branch(training);
+    refresh_training_branch(branch);
+    evaluate_branch(branch);
 
-    test_equals(as_float(a), 3.0);
+    test_equals(to_float(a), 2.0);
 }
 
 void train_sin()
@@ -72,67 +71,6 @@ void train_sin()
     Branch training;
     //generate_feedback(training, b, training.eval("1.0"));
     evaluate_branch(training);
-}
-
-void test_refresh_training_branch()
-{
-    Branch branch;
-
-    Term* a = branch.eval("a = 1.0");
-    Term* b = branch.eval("b = 2.0");
-    branch.eval("c = add(a, b)");
-    branch.eval("feedback(c, 4.0)");
-    a->boolProp("trainable") = true;
-    b->boolProp("trainable") = true;
-
-    refresh_training_branch(branch);
-
-    test_assert(branch.contains(TRAINING_BRANCH_NAME));
-    Branch& trainingBranch = as_branch(branch[TRAINING_BRANCH_NAME]);
-
-    bool foundAssignToA = false;
-    bool foundAssignToB = false;
-    for (int i=0; i < trainingBranch.length(); i++) {
-        Term* term = trainingBranch[i];
-        if (term->function == ASSIGN_FUNC) {
-            if (term->input(0) == a)
-                foundAssignToA = true;
-            else if (term->input(0) == b)
-                foundAssignToB = true;
-            else
-                // there shouldn't be any other assign()s in there
-                test_assert(false);
-        }
-    }
-    test_assert(foundAssignToA);
-    test_assert(foundAssignToB);
-
-    // now run our branch
-    evaluate_branch(branch);
-
-    test_assert(a->asFloat() == 1.5);
-    test_assert(b->asFloat() == 2.5);
-}
-
-void test_refresh_training_branch2()
-{
-    // make sure that refresh_training_branch is properly normalizing assign() functions
-    Branch branch;
-    branch.eval("a = 1.0");
-    branch.eval("feedback(a, 2.0)");
-    branch.eval("feedback(a, 3.0)");
-    refresh_training_branch(branch);
-
-    Branch& tbranch = as_branch(branch[TRAINING_BRANCH_NAME]);
-    int assignCount = 0;
-
-    for (int i=0; i < tbranch.length(); i++) {
-        if (tbranch[i]->function == ASSIGN_FUNC)
-            assignCount++;
-    }
-
-    test_assert(assignCount > 0);
-    test_assert(assignCount == 1);
 }
 
 void feedback_across_function()
@@ -172,7 +110,7 @@ void register_tests()
 {
     REGISTER_TEST_CASE(feedback_tests::train_addition1);
     REGISTER_TEST_CASE(feedback_tests::train_addition2);
-    //REGISTER_TEST_CASE(feedback_tests::train_mult);
+    REGISTER_TEST_CASE(feedback_tests::train_mult);
     //REGISTER_TEST_CASE(feedback_tests::train_sin);
     //FIXME REGISTER_TEST_CASE(feedback_tests::test_refresh_training_branch);
     //FIXME REGISTER_TEST_CASE(feedback_tests::test_refresh_training_branch2);
