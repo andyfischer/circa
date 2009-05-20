7c04a203378dc5dc3bf4dc04bed6a485c81d1664
Paul Hodge <paul.hodge.email@gmail.com>
2009-05-20 16:55:29 -0400

    Add refactoring_tests.cpp

---
+++ src/tests/refactoring_tests.cpp
@@ -0,0 +1,45 @@
+// Copyright 2008 Paul Hodge
+
+#include <circa.h>
+
+namespace circa {
+namespace refactoring_tests {
+
+void _empty_evaluate(Term*) {}
+
+void repro_source_after_rename()
+{
+    Branch branch;
+
+    // Simple test
+    Term* a = branch.eval("a = 1");
+
+    rename(a, "apple");
+
+    test_equals(get_term_source(a), "apple = 1");
+
+    // Rename a function argument
+    Term* b = branch.eval("b = 5");
+    Term* add = branch.eval("add( b , 10)");
+
+    rename(b, "banana");
+
+    test_equals(get_term_source(add), "add( banana , 10)");
+
+    // Rename a function
+    Term* myfunc = import_function(branch, _empty_evaluate, "def myfunc(int)");
+    Term* myfunc_call = branch.eval("myfunc(555)");
+
+    rename(myfunc, "my_renamed_func");
+
+    test_equals(get_term_source(myfunc_call), "my_renamed_func(555)");
+}
+
+void register_tests()
+{
+    REGISTER_TEST_CASE(refactoring_tests::repro_source_after_rename);
+}
+
+} // namespace refactoring_tests
+
+} // namespace circa
