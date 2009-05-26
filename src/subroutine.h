815a3ff0bde98f177d90fbda760a907a92ab6b34
Paul Hodge <paul.hodge.email@gmail.com>
2009-05-25 22:43:37 -0700

    Minor refactor of subroutine code

--- src/subroutine.h
+++ src/subroutine.h
@@ -12,10 +12,11 @@ namespace subroutine_t {
 bool is_subroutine(Term* term);
 Function& get_subroutines_function_def(Term* term);
 void initialize_subroutine(Term* term);
-void initialize_subroutine_state(Term* term, Branch& state);
 Branch& call_subroutine(Branch& branch, std::string const& functionName);
 void subroutine_call_evaluate(Term* caller);
-Branch& get_state_for_subroutine_call(Term* term);
+Term* get_state_for_subroutine_call(Term* term);
+bool is_subroutine_state_expanded(Term* term);
+void expand_subroutines_hidden_state(Term* term, Term* state);
 
 } // namespace circa
 
