8307a545601d1eba1c1178e00baf03d236ef59ff
Andy Fischer <andy.fischer@gmail.com>
2009-06-03 18:41:30 -0400

    Turn sub() into an overloaded func

--- src/builtin_functions/sub.cpp
+++ src/builtin_functions/sub.cpp
@@ -5,15 +5,27 @@
 namespace circa {
 namespace sub_function {
 
-    void evaluate(Term* caller)
+    void evaluate_i(Term* caller)
+    {
+        as_int(caller) = as_int(caller->input(0)) - as_int(caller->input(1));
+    }
+
+    void evaluate_f(Term* caller)
     {
         as_float(caller) = to_float(caller->input(0)) - to_float(caller->input(1));
     }
 
     void setup(Branch& kernel)
     {
-        SUB_FUNC = import_function(kernel, evaluate, "sub(float,float) : float");
-        as_function(SUB_FUNC).pureFunction = true;
+        SUB_FUNC = create_overloaded_function(&kernel, "sub");
+
+        Term* sub_i = import_function_overload(SUB_FUNC, evaluate_i, "sub_i(int,int):int");
+        as_function(sub_i).pureFunction = true;
+        Term* sub_f = import_function_overload(SUB_FUNC, evaluate_f, "sub_f(int,int):int");
+        as_function(sub_f).pureFunction = true;
+
+        kernel.bindName(sub_i, "sub_i");
+        kernel.bindName(sub_f, "sub_f");
     }
 }
 } // namespace circa
