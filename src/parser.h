7190c76bc54ca14aee1084c990457030e9c12c35
Paul Hodge <paul.hodge.email@gmail.com>
2009-06-10 14:44:46 -0400

    Add unary negation

--- src/parser.h
+++ src/parser.h
@@ -28,6 +28,7 @@ Term* expression_statement(Branch& branch, TokenStream& tokens);
 Term* return_statement(Branch& branch, TokenStream& tokens);
 Term* infix_expression(Branch& branch, TokenStream& tokens);
 Term* infix_expression_nested(Branch& branch, TokenStream& tokens, int precedence);
+Term* unary_expression(Branch& branch, TokenStream& tokens);
 Term* dot_expression(Branch& branch, TokenStream& tokens);
 Term* atom(Branch& branch, TokenStream& tokens);
 Term* function_call(Branch& branch, TokenStream& tokens);
