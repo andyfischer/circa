89b5d11c77675fd73f70f7fe9925df39afef8a2d
Paul Hodge <paul.hodge.email@gmail.com>
2009-06-13 11:56:15 -0700

    Add rand_i(), fix code generation for a[i] = b

--- src/builtin_functions/rand.cpp
+++ src/builtin_functions/rand.cpp
@@ -7,21 +7,43 @@
 namespace circa {
 namespace rand_function {
 
-    void evaluate(Term* caller)
-    {
-        static bool seeded = false;
+    bool seeded = false;
 
+    void seed_if_needed()
+    {
         if (!seeded) {
             srand((unsigned) time(0));
             seeded = true;
         }
+    }
 
+    void evaluate(Term* caller)
+    {
+        seed_if_needed();
         as_float(caller) = (float) rand() / 0x8fffffff;
+    }
+
+    void evaluate_i(Term* caller)
+    {
+        seed_if_needed();
+
+        as_int(caller) = rand();
+    }
+
+    void evaluate_i_i(Term* caller)
+    {
+        seed_if_needed();
+        int period = caller->input(0)->asInt();
+
+        // TODO: replace this, builtin rand() does not have good randomness in lower bits.
+        as_int(caller) = rand() % period;
     }
 
     void setup(Branch& kernel)
     {
         import_function(kernel, evaluate, "rand() : float");
+        import_function(kernel, evaluate_i, "rand_i() : int");
+        //import_function(kernel, evaluate_i_i, "rand_i(int) : int");
     }
 }
 } // namespace circa
