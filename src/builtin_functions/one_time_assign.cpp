b1bb52ee7930d98a34fa6e6f42816907a2909098
Paul Hodge <paul.hodge.email@gmail.com>
2009-06-17 16:41:16 -0400

    Fix source repro of state i = x

--- src/builtin_functions/one_time_assign.cpp
+++ src/builtin_functions/one_time_assign.cpp
@@ -20,19 +20,11 @@ namespace one_time_assign_function {
         return caller->input(1)->type;
     }
 
-    std::string toSourceString(Term* term)
-    {
-        std::stringstream result;
-        result << "state " << term->name << " = " << get_source_of_input(term, 1);
-        return result.str();
-    }
-
     void setup(Branch& kernel)
     {
         ONE_TIME_ASSIGN_FUNC = import_function(kernel, evaluate,
                 "one_time_assign(state bool, any) : any");
         function_get_specialize_type(ONE_TIME_ASSIGN_FUNC) = specializeType;
-        function_get_to_source_string(ONE_TIME_ASSIGN_FUNC) = toSourceString;
     }
 }
 }
