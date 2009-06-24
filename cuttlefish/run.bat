11c1bb94515db3986bc35ca18f05becb37e427fb
Andy Fischer <andy.fischer@gmail.com>
2009-06-24 16:54:53 -0400

    Windows build fixes
    - Add SDL_ttf and SDL_mixer
    - Build with /MD instead of /MT (I think this is required by LGPL)
    - Vague support for debug vs release builds

--- cuttlefish/run.bat
+++ cuttlefish/run.bat
@@ -1,4 +1,4 @@
 
 set PATH=%PATH%;%~dp0\..\SDL_deps\bin
 
-%~dp0\..\build\bin\cfish.exe %*
+%~dp0\..\build\bin\cfish.exe %1.ca
--- cuttlefish/src/sdl_wrapper.cpp
+++ cuttlefish/src/sdl_wrapper.cpp
@@ -1,7 +1,6 @@
 // Copyright 2008 Andrew Fischer
 
 #include "SDL.h"
-#include "SDL_gfxPrimitives.h"
 
 #include "circa.h"
 
@@ -11,6 +10,7 @@ extern int SCREEN_HEIGHT;
 
 namespace sdl_wrapper {
 
+/*
 void box(circa::Term* caller)
 {
     boxColor(SCREEN,
@@ -74,9 +74,11 @@ void drawText(circa::Term* caller)
     stringColor(SCREEN, caller->input(0)->asFloat(), caller->input(1)->asFloat(),
             caller->input(2)->asString().c_str(), caller->input(3)->asInt());
 }
+*/
 
 void register_functions(circa::Branch& branch)
 {
+    /*
     circa::import_function(branch, box, "box(float,float,float,float,int)");
     circa::import_function(branch, line, "sdl_line(float,float,float,float,int)");
     circa::import_function(branch, circle, "circle(float,float,float,int)");
@@ -84,6 +86,7 @@ void register_functions(circa::Branch& branch)
     circa::import_function(branch, background, "background(int)");
     circa::import_function(branch, shape, "shape(List,int)");
     circa::import_function(branch, drawText, "drawText(float,float,string,int)");
+    */
 }
 
 } // namespace sdl_wrapper
