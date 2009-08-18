1778e1c450967c00d2e132f401f665af4179fbc4
Paul Hodge <paul.hodge.email@gmail.com>
2009-08-18 10:54:53 -0700

    Refactor: move some stuff into new file builtin_types.cpp

---
+++ src/builtin_types.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2007-2009 Paul Hodge. All rights reserved.
+
+#ifndef CIRCA_BUILTIN_TYPES_INCLUDED
+#define CIRCA_BUILTIN_TYPES_INCLUDED
+
+namespace circa {
+
+namespace set_t {
+    void add(Branch& branch, Term* value);
+}
+
+namespace list_t {
+    void append(Branch& branch, Term* value);
+    std::string to_string(Term* caller);
+}
+
+namespace dict_t {
+    std::string to_string(Branch& branch);
+}
+
+void setup_builtin_types(Branch& kernel);
+
+}
+
+#endif
