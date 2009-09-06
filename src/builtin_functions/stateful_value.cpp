45a458b5026f3e0d2f6cb143c4b4137f47d266ee
Andy Fischer <andy.fischer@gmail.com>
2009-09-06 09:57:17 -0700

    Fix source repro bug with state i = <name>

--- src/builtin_functions/stateful_value.cpp
+++ src/builtin_functions/stateful_value.cpp
@@ -9,9 +9,32 @@ namespace stateful_value_function {
     {
     }
 
+    std::string toSourceString(Term* term)
+    {
+        std::stringstream out;
+        out << "state ";
+
+        if (term->hasProperty("syntaxHints:explicitType"))
+            out << term->stringProp("syntaxHints:explicitType") << " ";
+
+        out << term->name;
+
+        if (term->hasProperty("initializedBy")) {
+            Term* initializedBy = term->refProp("initializedBy");
+            out << " = ";
+            if (initializedBy->name != "")
+                out << initializedBy->name;
+            else
+                out << get_term_source(initializedBy);
+        }
+
+        return out.str();
+    }
+
     void setup(Branch& kernel)
     {
         STATEFUL_VALUE_FUNC = import_function(kernel, evaluate, "stateful_value() : any");
+        function_t::get_to_source_string(STATEFUL_VALUE_FUNC) = toSourceString;
     }
 }
 }
