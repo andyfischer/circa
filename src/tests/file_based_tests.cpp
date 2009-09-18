76a979a0da2c202eee7ae2c252ab566e738818af
Paul Hodge <paul.hodge.email@gmail.com>
2009-09-17 23:05:09 -0700

    Add tests for include() function, fix a dumb bug that caused crashes when include() reloaded a script that had errors

--- src/tests/file_based_tests.cpp
+++ src/tests/file_based_tests.cpp
@@ -5,6 +5,8 @@
 namespace circa {
 namespace file_based_tests {
 
+// Class that implements FileIORedirector. This class also installs itself on construction
+// and deletes itself afterwards.
 struct FakeFileSystem : FileIORedirector
 {
     struct File {
@@ -15,6 +17,9 @@ struct FakeFileSystem : FileIORedirector
     };
 
     std::map<std::string, File> _files;
+
+    FakeFileSystem() { install_fake_file_io(this); }
+    ~FakeFileSystem() { install_fake_file_io(NULL); }
 
     std::string& operator [] (std::string const& filename)
     {
@@ -41,31 +46,65 @@ struct FakeFileSystem : FileIORedirector
         return _files[filename].last_modified;
     }
 
-} FakeFileSystem;
-
-struct FakeFileSystemInstaller
-{
-    FakeFileSystemInstaller() { install_fake_file_io(&FakeFileSystem); }
-    ~FakeFileSystemInstaller() { install_fake_file_io(NULL); }
 };
 
 void test_the_test()
 {
     // Make sure that the file IO redirection thingy is working.
 
-    FakeFileSystemInstaller installer;
+    FakeFileSystem files;
 
-    FakeFileSystem["a.txt"] = "hello";
+    files["a.txt"] = "hello";
     test_assert(read_text_file("a.txt") == "hello");
 
     write_text_file("a.txt", "goodbye");
     test_assert(read_text_file("a.txt") == "goodbye");
-    test_assert(FakeFileSystem["a.txt"] == "goodbye");
+    test_assert(files["a.txt"] == "goodbye");
+}
+
+void test_include_function()
+{
+    FakeFileSystem files;
+
+    files["file.ca"] = "a = 1";
+
+    // Basic test, load our fake file.
+    Branch branch;
+    Term* incl = branch.eval("include('file.ca')");
+
+    test_assert(is_branch(incl));
+    test_assert(incl->asBranch()["a"]->asInt() == 1);
+
+    // Next, modify the file and reload.
+    files["file.ca"] = "b = 2";
+    files.last_modified("file.ca")++;
+
+    evaluate_term(incl);
+
+    test_assert(!incl->asBranch().contains("a"));
+    test_assert(incl->asBranch()["b"]->asInt() == 2);
+
+    // Modify the file but don't modify the last_modified time, make sure that
+    // it doesn't reload this time.
+    files["file.ca"] = "c = 3";
+    evaluate_term(incl);
+    test_assert(!incl->asBranch().contains("a"));
+    test_assert(!incl->asBranch().contains("c"));
+    test_assert(incl->asBranch()["b"]->asInt() == 2);
+
+    // Modify the file with a script that has errors
+    files["file.ca"] = "add(what what)";
+    files.last_modified("file.ca")++;
+
+    evaluate_term(incl);
+
+    test_assert(has_static_errors(incl->asBranch()));
 }
 
 void register_tests()
 {
     REGISTER_TEST_CASE(file_based_tests::test_the_test);
+    REGISTER_TEST_CASE(file_based_tests::test_include_function);
 }
 
 } // namespace file_based_tests
