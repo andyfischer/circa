05f72bbb130437d1689ad585631b08a1fe9eaa5e
Paul Hodge <paul.hodge.email@gmail.com>
2009-09-17 22:38:38 -0700

    Add a new test suite for testing things that read/write files

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -1,11 +1,18 @@
 // Copyright (c) 2007-2009 Paul Hodge. All rights reserved.
 
+#include <sys/stat.h>
+
 #include "circa.h"
 
 namespace circa {
 
+FileIORedirector* FAKE_FILE_IO = NULL;
+
 std::string read_text_file(std::string const& filename)
 {
+    if (FAKE_FILE_IO != NULL)
+        return FAKE_FILE_IO->read_text_file(filename);
+
     std::ifstream file;
     file.open(filename.c_str(), std::ios::in);
     std::stringstream contents;
@@ -23,10 +30,29 @@ std::string read_text_file(std::string const& filename)
 
 void write_text_file(std::string const& filename, std::string const& contents)
 {
+    if (FAKE_FILE_IO != NULL) {
+        FAKE_FILE_IO->write_text_file(filename, contents);
+        return;
+    }
+
     std::ofstream file;
     file.open(filename.c_str(), std::ios::out | std::ios::binary);
     file << contents;
     file.close();
+}
+
+time_t get_modified_time(std::string const& filename)
+{
+    if (FAKE_FILE_IO != NULL)
+        return FAKE_FILE_IO->get_modified_time(filename);
+
+    struct stat s;
+
+    s.st_mtime = 0;
+
+    stat(filename.c_str(), &s);
+
+    return s.st_mtime;
 }
 
 std::string get_directory_for_filename(std::string const& filename)
@@ -43,6 +69,11 @@ std::string get_directory_for_filename(std::string const& filename)
     std::string result = filename.substr(0, last_slash);
 
     return result;
+}
+
+void install_fake_file_io(FileIORedirector* fakeObject)
+{
+    FAKE_FILE_IO = fakeObject;
 }
     
 } // namespace circa
---
+++ src/tests/file_based_tests.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2007-2009 Paul Hodge. All rights reserved.
+
+#include "circa.h"
+
+namespace circa {
+namespace file_based_tests {
+
+struct FakeFileSystem : FileIORedirector
+{
+    struct File {
+        std::string contents;
+        time_t last_modified;
+
+        File() : last_modified(0) {}
+    };
+
+    std::map<std::string, File> _files;
+
+    std::string& operator [] (std::string const& filename)
+    {
+        return _files[filename].contents;
+    }
+
+    time_t& last_modified(std::string const& filename)
+    {
+        return _files[filename].last_modified;
+    }
+
+    virtual std::string read_text_file(std::string const& filename)
+    {
+        return _files[filename].contents;
+    }
+
+    virtual void write_text_file(std::string const& filename, std::string const& contents)
+    {
+        _files[filename].contents = contents;
+    }
+
+    virtual time_t get_modified_time(std::string const& filename)
+    {
+        return _files[filename].last_modified;
+    }
+
+} FakeFileSystem;
+
+struct FakeFileSystemInstaller
+{
+    FakeFileSystemInstaller() { install_fake_file_io(&FakeFileSystem); }
+    ~FakeFileSystemInstaller() { install_fake_file_io(NULL); }
+};
+
+void test_the_test()
+{
+    // Make sure that the file IO redirection thingy is working.
+
+    FakeFileSystemInstaller installer;
+
+    FakeFileSystem["a.txt"] = "hello";
+    test_assert(read_text_file("a.txt") == "hello");
+
+    write_text_file("a.txt", "goodbye");
+    test_assert(read_text_file("a.txt") == "goodbye");
+    test_assert(FakeFileSystem["a.txt"] == "goodbye");
+}
+
+void register_tests()
+{
+    REGISTER_TEST_CASE(file_based_tests::test_the_test);
+}
+
+} // namespace file_based_tests
+
+} // namespace circa
