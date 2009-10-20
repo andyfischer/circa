9ec01c80e0a5bc0dbdd588c54f5c139756feb26a
Paul Hodge <paul.hodge.email@gmail.com>
2009-10-19 18:42:44 -0700

    plastic: First postprocess filter works

--- plastic/src/postprocess.cpp
+++ plastic/src/postprocess.cpp
@@ -24,7 +24,7 @@ namespace postprocess_functions
         }
     };
     
-    void hidden_surface(Term* caller)
+    void make_surface(Term* caller)
     {
         gl_clear_error();
 
@@ -67,71 +67,65 @@ namespace postprocess_functions
         assign_value(caller->input(0), caller);
     }
 
-    void get_tex_id_for_main_surface(Term* caller)
-    {
-        int& tex_id = as_int(caller->input(0));
-
-        if (tex_id == 0) {
-            glGenTextures(1, (GLuint*) &tex_id);
-            glBindTexture(GL_TEXTURE_2D, tex_id);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-                    WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glBindTexture(GL_TEXTURE_2D, 0);
-
-            glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
-                    GL_TEXTURE_2D, tex_id, 0);
-        }
-        as_int(caller) = tex_id;
-    }
-
-    void copy_surface(Term* caller)
+    void draw_surface(Term* caller)
     {
         Surface source_surface(caller->input(0));
-        Surface dest_surface(caller->input(1));
-        float alpha = float_input(caller, 2);
-
-        // bind surface
-        glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, dest_surface.fbo_id);
-        //glViewport(0, 0, dest_surface.width, dest_surface.height);
-        //glMatrixMode(GL_PROJECTION);
-        //glOrtho(0, dest_surface.width, dest_surface.height, 0, -1000.0f, 1000.0f);
-        //glMatrixMode(GL_MODELVIEW);
-        //glLoadIdentity();
+        float alpha = float_input(caller, 1);
 
         glEnable(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, dest_surface.tex_id);
+        glBindTexture(GL_TEXTURE_2D, source_surface.tex_id);
 
         glColor4f(1,1,1,alpha);
         
         glBegin(GL_QUADS);
-        glTexCoord2i(0, 0); glVertex2i(-1, -1);
-        glTexCoord2i(1, 0); glVertex2i(1, -1);
-        glTexCoord2i(1, 1); glVertex2i(1, 1);
-        glTexCoord2i(0, 1); glVertex2i(-1, 1);
+        glTexCoord2i(0, 1); glVertex2i(0, 0);
+        glTexCoord2i(1, 1); glVertex2i(source_surface.width, 0);
+        glTexCoord2i(1, 0); glVertex2i(source_surface.width, source_surface.height);
+        glTexCoord2i(0, 0); glVertex2i(0, source_surface.height);
         glEnd();
 
-        gl_check_error(caller);
-
+        // Reset state
         glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
         glBindTexture(GL_TEXTURE_2D, 0);
-
-        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-        //glMatrixMode(GL_PROJECTION);
-        //glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, -1000.0f, 1000.0f);
-        //glMatrixMode(GL_MODELVIEW);
-        //glLoadIdentity();
         glColor4f(1,1,1,1);
+
+        gl_check_error(caller);
+    }
+
+    void bind_surface(Term* caller)
+    {
+        Surface surface(caller->input(0));
+        glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, surface.fbo_id);
+        glViewport(0, 0, surface.width, surface.height);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, surface.width, surface.height, 0, -1000.0f, 1000.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        gl_check_error(caller);
+    }
+
+    void bind_main_surface(Term* caller)
+    {
+        glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, -1000.0f, 1000.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        gl_check_error(caller);
     }
 
     void setup(Branch& kernel)
     {
         Branch& postprocess_ns = kernel["postprocess"]->asBranch();
-        install_function(postprocess_ns["hidden_surface"], hidden_surface);
-        install_function(postprocess_ns["get_tex_id_for_main_surface"],
-                get_tex_id_for_main_surface);
-        install_function(postprocess_ns["copy_surface"], copy_surface);
+        install_function(postprocess_ns["make_surface"], make_surface);
+        install_function(postprocess_ns["draw_surface"], draw_surface);
+        install_function(postprocess_ns["bind_surface"], bind_surface);
+        install_function(postprocess_ns["bind_main_surface"], bind_main_surface);
     }
 
 } // namespace postprocess_functions
--- src/external_api.cpp
+++ src/external_api.cpp
@@ -4,23 +4,34 @@
 
 namespace circa {
 
+void assert_input_index(Term* term, int index)
+{
+    if (index >= term->numInputs()) {
+        throw std::runtime_error("Input index out of range");
+    }
+}
+
 int int_input(Term* term, int index)
 {
+    assert_input_index(term, index);
     return as_int(term->input(index));
 }
 
 float float_input(Term* term, int index)
 {
+    assert_input_index(term, index);
     return to_float(term->input(index));
 }
 
 bool bool_input(Term* term, int index)
 {
+    assert_input_index(term, index);
     return as_bool(term->input(index));
 }
 
 const char* string_input(Term* term, int index)
 {
+    assert_input_index(term, index);
     return as_string(term->input(index)).c_str();
 }
 
