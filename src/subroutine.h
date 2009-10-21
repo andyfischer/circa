4ec57f25022fc8acbc0fcb67f2667bd18223999f
Andy Fischer <andy.fischer@gmail.com>
2009-10-21 10:03:42 -0700

    Misc refactoring/cleanup around subroutines

--- src/subroutine.h
+++ src/subroutine.h
@@ -7,12 +7,15 @@ namespace circa {
 
 namespace subroutine_t {
     std::string to_string(Term* term);
+    void evaluate(Term* caller);
 }
 
 bool is_subroutine(Term* term);
-Branch& call_subroutine(Branch& branch, std::string const& functionName);
+
+// Perform various steps to finish creating a subroutine
+void finish_building_subroutine(Term* sub, Term* outputType);
+
 void subroutine_update_hidden_state_type(Term* sub);
-void subroutine_call_evaluate(Term* caller);
 bool is_subroutine_state_expanded(Term* term);
 void expand_subroutines_hidden_state(Term* call, Term* state);
 
