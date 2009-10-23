c9daa0b749cecb44b59a7d9183c2b43701eb59cb
Paul Hodge <paul.hodge.email@gmail.com>
2009-10-23 09:40:03 -0700

    Add file_changed() function

--- src/builtin_functions/include.cpp
+++ src/builtin_functions/include.cpp
@@ -7,25 +7,16 @@ namespace include_function {
 
     void load_script(Term* caller)
     {
-        Term* state = caller->input(0);
-        std::string &prev_filename = state->asBranch()[0]->asString();
-        int &prev_modified = state->asBranch()[1]->asInt();
+        Term* fileSignature = caller->input(0);
         Branch& contents = as_branch(caller);
 
         std::string &requested_filename = caller->input(1)->asString();
-
         std::string actual_filename = get_path_relative_to_source(caller, requested_filename);
 
-        time_t modifiedTime = get_modified_time(actual_filename);
-
         // Reload if the filename or modified-time has changed
-        if (requested_filename != prev_filename
-                || prev_modified != modifiedTime
+        if (file_changed_function::check(fileSignature, actual_filename)
                 || contents.length() == 0)
         {
-            prev_filename = requested_filename;
-            prev_modified = (int) modifiedTime;
-
             Branch previous_contents;
             duplicate_branch(contents, previous_contents);
 
@@ -59,22 +50,13 @@ namespace include_function {
         evaluate_branch(contents, caller);
     }
 
-#if 0
-    std::string toSourceString(Term* term)
-    {
-        return "include " + get_source_of_input(term, 1);
-    }
-#endif
-
     void setup(Branch& kernel)
     {
-        parse_type(kernel, "type _file_signature { string filename, int time_modified }");
         INCLUDE_FUNC = import_function(kernel, evaluate_include,
-                "include(state _file_signature, string filename) :: Branch");
-        //function_t::get_to_source_string(INCLUDE_FUNC) = toSourceString;
+                "include(state FileSignature, string filename) :: Branch");
 
         import_function(kernel, load_script,
-            "load_script(state _file_signature, string filename) :: Branch");
+            "load_script(state FileSignature, string filename) :: Branch");
     }
 }
 } // namespace circa
