ba7817b78ab1a321d699f19acc660af34485015c
Paul Hodge <paul.hodge.email@gmail.com>
2009-11-30 22:04:13 -0800

    Clean up dusty old test scripts

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -83,3 +83,35 @@ def compare_command_against_file(command, filename):
 
     return True
     
+class TestCase(object):
+    def __init__(self, name, func):
+        self.name = name
+        self.func = func
+
+class TestFailure(object):
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+
+def run_tests_and_print_results(tests):
+    test_count = 0
+    failures = []
+
+    for test in tests:
+        test_count += 1
+        result = test.func()
+
+        if result is not True:
+            failures.append(TestFailure(test.name, result))
+
+    plural = "s" if test_count != 1 else ""
+    if not failures:
+        print "Ran %i test%s. All tests passed." % (test_count, plural)
+    else:
+        print "Ran %i test%s. %i failures.." % (test_count, plural, len(failures))
+
+        for failure in failures:
+            comment = ""
+            if failure.message: comment = ": "+failure.message
+            print "%s failed%s" % (failure.name, comment)
+
