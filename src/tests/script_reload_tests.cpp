7c289c8191744eab03f83fed5e958748feb939b8
Paul Hodge <paul.hodge.email@gmail.com>
2010-03-04 10:16:47 -0800

    Fix faulty test_function_change

--- src/tests/script_reload_tests.cpp
+++ src/tests/script_reload_tests.cpp
@@ -27,7 +27,7 @@ struct ReloadHelper
         evaluate_branch(branch, &errorListener);
         if (has_runtime_error(&errorListener)) {
             std::cout << "Error in reload test: " << get_current_test_name() << std::endl;
-            std::cout << get_error_message(&errorListener) << std::endl;
+            std::cout << get_error_message(branch) << std::endl;
             dump_branch(branch);
             declare_current_test_failed();
         }
@@ -49,13 +49,15 @@ void test_simple_with_state()
     ReloadHelper helper;
     helper.load("state List l; l.append(1)");
     helper.load("state List l; l.append(2)");
+    test_assert(as_int(as_branch(helper.branch["l"])[0]) == 1);
+    test_assert(as_int(as_branch(helper.branch["l"])[1]) == 2);
 }
 
 void test_function_change()
 {
     ReloadHelper helper;
     helper.load("def f(int i); add(i,i) end; f(5)");
-    helper.load("def f(float i); mult(i,i+1) end; f(5)");
+    helper.load("def f(number i); mult(i,i+1) end; f(5)");
 }
 
 void test_with_custom_type()
@@ -69,7 +71,7 @@ void register_tests()
 {
     REGISTER_TEST_CASE(script_reload_tests::test_simple);
     REGISTER_TEST_CASE(script_reload_tests::test_simple_with_state);
-    //REGISTER_TEST_CASE(script_reload_tests::test_function_change);
+    REGISTER_TEST_CASE(script_reload_tests::test_function_change);
     REGISTER_TEST_CASE(script_reload_tests::test_with_custom_type);
 }
 
