11a0aa9fde46e53c63743ea328b50e1adbc0d327
Paul Hodge <paul.hodge.email@gmail.com>
2010-03-31 20:43:30 -0700

    move int_t and float_t to builtin_types folder

---
+++ src/builtin_types/float.cpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2007-2010 Paul Hodge. All rights reserved.
+
+namespace circa {
+namespace float_t {
+    void initialize(Type* type, TaggedValue* value)
+    {
+        set_float(value, 0);
+    }
+
+    void cast(Type* type, TaggedValue* source, TaggedValue* dest)
+    {
+        set_float(dest, to_float(source));
+    }
+
+    bool cast_possible(Type* source, TaggedValue* value)
+    {
+        return is_int(value) || is_float(value);
+    }
+
+    bool equals(TaggedValue* a, TaggedValue* b)
+    {
+        return to_float(a) == to_float(b);
+    }
+    std::string to_string(TaggedValue* value)
+    {
+        std::stringstream out;
+        out << as_float(value);
+        return out.str();
+    }
+    bool matches_type(Type* type, Term* term)
+    {
+        return term->type == FLOAT_TYPE || term->type == INT_TYPE;
+    }
+
+    std::string to_source_string(Term* term)
+    {
+        // Correctly formatting floats is a tricky problem.
+
+        // First, check if we know how the user formatted this number. If this value
+        // still has the exact same value, then use the original formatting.
+        if (term->hasProperty("float:original-format")) {
+            std::string const& originalFormat = term->stringProp("float:original-format");
+            float actual = as_float(term);
+            float original = (float) atof(originalFormat.c_str());
+            if (actual == original) {
+                return originalFormat;
+            }
+        }
+
+        // Otherwise, format the current value with naive formatting. This could be
+        // improved; we could try harder to recreate some of the original formatting.
+        std::stringstream strm;
+        strm << as_float(term);
+
+        if (term->floatPropOptional("mutability", 0.0) > 0.5)
+            strm << "?";
+
+        std::string result = strm.str();
+
+        // Check this string and make sure there is a decimal point. If not, append one.
+        bool decimalFound = false;
+        for (unsigned i=0; i < result.length(); i++)
+            if (result[i] == '.')
+                decimalFound = true;
+
+        if (!decimalFound)
+            return result + ".0";
+        else
+            return result;
+    }
+    void format_source(StyledSource* source, Term* term)
+    {
+        append_phrase(source, float_t::to_source_string(term).c_str(), term, token::FLOAT_TOKEN);
+    }
+    void setup_type(Type* type)
+    {
+        reset_type(type);
+        type->name = "number";
+        type->cast = cast;
+        type->castPossible = cast_possible;
+        type->equals = equals;
+        type->matchesType = matches_type;
+        type->toString = to_string;
+        type->formatSource = format_source;
+    }
+}
+}
---
+++ src/builtin_types/int.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2007-2010 Paul Hodge. All rights reserved.
+
+namespace circa {
+namespace int_t {
+    bool equals(TaggedValue* a, TaggedValue* b)
+    {
+        if (is_float(b))
+            return float_t::equals(a,b);
+        if (!is_int(b))
+            return false;
+        return as_int(a) == as_int(b);
+    }
+    std::string to_string(TaggedValue* value)
+    {
+        std::stringstream strm;
+        strm << as_int(value);
+        return strm.str();
+    }
+    void format_source(StyledSource* source, Term* term)
+    {
+        std::stringstream strm;
+        if (term->stringPropOptional("syntax:integerFormat", "dec") == "hex")
+            strm << "0x" << std::hex;
+
+        strm << as_int(term);
+        append_phrase(source, strm.str(), term, token::INTEGER);
+    }
+    void setup_type(Type* type)
+    {
+        reset_type(type);
+        type->name = "int";
+        type->equals = equals;
+        type->toString = to_string;
+        type->formatSource = format_source;
+    }
+}
+}
