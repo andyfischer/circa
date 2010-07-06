9a8699ebe6e74c5f8155aa4697c43874b48babe1
Paul Hodge <paul.hodge.email@gmail.com>
2010-07-06 10:20:23 -0700

    Add trace() (alias for print), fix draw_rect

--- src/builtin_functions/print.cpp
+++ src/builtin_functions/print.cpp
@@ -20,6 +20,10 @@ namespace print_function {
     {
         import_function(kernel, evaluate, "print(any...) "
                 "'Prints a line of text output to the console' end");
+
+        // Alias
+        import_function(kernel, evaluate, "trace(any...) "
+                "'Prints a line of text output to the console' end");
     }
 }
 } // namespace circa
