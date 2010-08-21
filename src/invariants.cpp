bc5d30cf4364248cd62b69c885e1cde6f8e5d84b
Paul Hodge <paul.hodge.email@gmail.com>
2010-08-21 15:08:35 -0700

    Bug fix in check_invariants

--- src/invariants.cpp
+++ src/invariants.cpp
@@ -12,17 +12,14 @@ bool check_invariants(Term* term, std::string& result)
 {
     if (term->value_type == NULL) {
         result = "TaggedValue has null type";
-        ca_assert(false);
         return false;
     }
 
     if (term->type != NULL
-            && (term->value_type != &as_type(term->type))
             && term->type != ANY_TYPE
-            && !is_branch(term)) {
+            && !value_fits_type(term, type_contents(term->type))) {
         result = "Value has wrong type: term->type is " + term->type->name + ", tag is "
             + term->value_type->name;
-        ca_assert(false);
         return false;
     }
 
