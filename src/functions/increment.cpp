5dc001f6808bf41d34dd0ea62ff2b06f5108cc7f
Paul Hodge <paul.hodge.email@gmail.com>
2010-09-20 20:39:29 -0400

    add increment() function

---
+++ src/functions/increment.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2007-2010 Paul Hodge. All rights reserved
+
+#include <circa.h>
+#include <importing_macros.h>
+
+namespace circa {
+namespace increment_function {
+    
+    CA_START_FUNCTIONS;
+
+    CA_DEFINE_FUNCTION(increment, "increment(int i) -> int")
+    {
+        make_int(OUTPUT, INT_INPUT(0) + 1);
+    }
+
+    CA_DEFINE_FUNCTION(increment, "decrement(int i) -> int")
+    {
+        make_int(OUTPUT, INT_INPUT(0) - 1);
+    }
+
+    void setup(Branch& kernel)
+    {
+        CA_SETUP_FUNCTIONS(kernel);
+    }
+}
+}
