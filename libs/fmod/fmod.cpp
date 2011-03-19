da5c39e7d7c978ba416dbd7977ab565e22ddaccd
Paul Hodge <paul.hodge.email@gmail.com>
2011-03-19 13:46:36 -0700

    Improve sound funcs (now load_sound is stateful)

--- libs/fmod/fmod.cpp
+++ libs/fmod/fmod.cpp
@@ -1,9 +1,11 @@
 
 #include "fmod.h"
+#include "fmod_errors.h"
 #include "circa.h"
+#include "importing_macros.h"
 
 FMOD_SYSTEM *g_system = NULL;
-
+circa::Type *g_soundType = NULL;
 
 void initialize()
 {
@@ -15,14 +17,35 @@ void initialize()
 
 CA_FUNCTION(load_sound)
 {
+    initialize();
+
+    FMOD_SOUND *sound;
+    FMOD_RESULT result = FMOD_System_CreateSound(g_system, circa::as_cstring(INPUT(0)),
+            FMOD_SOFTWARE, 0, &sound);
+
+    if (result != FMOD_OK) {
+        circa::error_occurred(CONTEXT, CALLER, FMOD_ErrorString(result));
+        circa::set_null(OUTPUT);
+    } else {
+        circa::set_pointer(OUTPUT, g_soundType, sound);
+    }
 }
 
 CA_FUNCTION(play_sound)
 {
+    initialize();
+
+    FMOD_RESULT result;
+
+    FMOD_SOUND *sound = (FMOD_SOUND*) INPUT(0)->value_data.ptr;
+
+    FMOD_CHANNEL *channel;
+    result = FMOD_System_PlaySound(g_system, FMOD_CHANNEL_FREE, sound, 0, &channel);
 }
 
 void setup(circa::Branch& kernel)
 {
-    circa::import_function(kernel, load_sound, "load_sound(string filename) -> Sound");
-    circa::import_function(kernel, play_sound, "play_sound(Sound sound)");
+    g_soundType = unbox_type(kernel["Sound"]);
+    circa::install_function(kernel["load_sound_int"], load_sound);
+    circa::install_function(kernel["play_sound"], play_sound);
 }
