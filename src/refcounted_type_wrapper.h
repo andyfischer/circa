6f5620afe62ccf11904acbabc97ccfeab83d2d99
Paul Hodge <paul.hodge.email@gmail.com>
2011-05-09 10:15:02 -0700

    Some more sanity in refcounted_type_wrapper

--- src/refcounted_type_wrapper.h
+++ src/refcounted_type_wrapper.h
@@ -19,6 +19,8 @@ namespace intrusive_refcounted {
     void release(Type*, TaggedValue* value)
     {
         T* instance = (T*) value->value_data.ptr;
+        ca_assert(instance != NULL);
+
         instance->_refCount--;
         if (instance->_refCount <= 0)
             delete instance;
@@ -28,19 +30,24 @@ namespace intrusive_refcounted {
     template <typename T>
     void set(TaggedValue* tv, Type* type, T* value)
     {
+        // Increase refcount first, in case the value already has this object.
+        value->_refCount++;
+
         set_null(tv);
         tv->value_type = type;
         tv->value_data = value;
-        value->_refCount++;
     }
 
     template <typename T>
-    void copy(Type*, TaggedValue* source, TaggedValue* dest)
+    void copy(Type* type, TaggedValue* source, TaggedValue* dest)
     {
-        set_null(dest);
-        dest->value_data = source->value_data;
-        dest->value_type = source->value_type;
-        ((T*) dest->value_data.ptr)->_refCount++;
+        // Increase refcount first, in case 'dest' already has this object.
+        T* instance = (T*) source->value_data.ptr;
+        ca_assert(instance->_refCount > 0);
+        instance->_refCount++;
+
+        change_type_no_initialize(dest, type);
+        dest->value_data.ptr = instance;
     }
 
     template <typename T>
