99552e76e479aa329a56d5a6c33de23ae07cbbf8
Paul Hodge <paul.hodge.email@gmail.com>
2012-05-02 17:11:00 -0700

    add a test that just reloads in a loop

---
+++ tests/embed/reload_loop.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) Andrew Fischer. See LICENSE file for license terms.
+
+#include <cstdio>
+
+#include "circa/circa.h"
+
+int main(int argc, char** argv)
+{
+    caWorld* world = circa_initialize();
+
+    circa_add_module_search_path(world, "tests/embed");
+
+    int iteration = 0;
+
+    while (true) {
+
+        caValue* value = circa_alloc_value();
+        circa_set_int(value, iteration);
+
+        circa_actor_run_message(world, "TestA", value);
+        sleep(1);
+        iteration++;
+    }
+
+    circa_shutdown(world);
+}
