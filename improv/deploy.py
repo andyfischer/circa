55ee2e14a173ea68f3ed02edbd525a6538dd0c0c
Paul Hodge <paul.hodge.email@gmail.com>
2012-05-27 20:08:40 -0700

    improv: add deploy.py script

---
+++ improv/deploy.py
@@ -0,0 +1,47 @@
+
+import os, shutil, zipfile
+
+AppName = 'ImprovAlpha4.app'
+
+def mkdir(path):
+    if os.path.exists(path):
+        return
+    os.mkdir(path)
+
+def rmdir(path):
+    if not os.path.exists(path):
+        return
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
+
+def copy(path, dest):
+
+    if os.path.isdir(path):
+        # Copy the whole folder, not just its contents
+        folderName = os.path.split(path)[1]
+        dest = os.path.join(dest, folderName)
+        rmdir(dest)
+        shutil.copytree(path, dest)
+    else:
+        mkdir(dest)
+        os.copy2(path,dest)
+
+
+# Setup common dirs
+#copy(AppName, macTempDir)
+#copy('ca', macTempDir)
+#copy('demos', macTempDir)
+
+macZip = zipfile.ZipFile('ImprovAlpha4.zip', 'w')
+
+def add(dir):
+    for root, dirnames, filenames in os.walk(dir):
+        for file in filenames:
+            fullpath = os.path.join(root,file)
+            macZip.write(fullpath, fullpath, zipfile.ZIP_DEFLATED)
+
+add('improv.app')
+add('ca')
+add('demos')
